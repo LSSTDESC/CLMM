bec0aa14788479da981961a02f0eec00452f8f74
Camille Avestruz <camille.avestruz@yale.edu>
2018-06-01 13:04:36 -0500

    Added info to the setup.py.  Will need to add more later

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='clmm',
+      version='0.1',
+      description='Calculate WL Masses and bias measured from simulation data',
+      url='http://github.com/storborg/funniest',
+      author='DESC-CL-WG',
+      author_email='avestruz@uchicago.edu',
+      license='MIT',
+      packages=['clmm'],
+      zip_safe=False)
