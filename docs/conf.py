0267f9cb8863d48776850ff6f986a021a1d4c0ba
matthewkirby <matthewkirby@email.arizona.edu>
2018-07-28 16:57:12 -0400

    Attempting to get auto docs

--- docs/conf.py
+++ docs/conf.py
@@ -23,6 +23,8 @@ import sys
 #sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('../clmm'))
 
+os.system('./apidoc.sh')
+
 
 # -- General configuration ------------------------------------------------
 
