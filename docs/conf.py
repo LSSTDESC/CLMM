0e23cc03bbf5719d47da94ac81b88e4f45fece71
matthewkirby <matthewkirby@email.arizona.edu>
2018-07-27 22:25:53 -0400

    Added the citation page. Added more temporary files to the gitignore. Updated path to library so that docs work

--- docs/conf.py
+++ docs/conf.py
@@ -19,8 +19,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../'))
+#sys.path.insert(0, os.path.abspath('.'))
+#sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../clmm'))
 
 
 # -- General configuration ------------------------------------------------
