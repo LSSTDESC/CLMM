78161481dba52445ecfd4a182fd9149dda920371
Alex Malz <aimalz@nyu.edu>
2018-07-28 18:02:12 -0400

    proposing a data format!

--- clmm/datatypes.py
+++ clmm/datatypes.py
@@ -1,9 +1,24 @@
-'''
-Define the data types used by GalaxyClusters
-'''
+"""
+Define the custom data type
+"""
 
+import astropy
+from astropy import table
+import collections
 from collections import namedtuple
 
-Position = namedtuple('Position', ['metadata', 'ra', 'dec'])
-Profile = namedtuple('Profile', ['metadata', 'distances', 'dimension'])
+"""
+Data: A namedtuple tying values with units to the metadata of where the values came from and thus how the values are used
 
+Parameters
+----------
+provenance: string, type of object that produced this data
+    e.g. Profile.NFW
+data: astropy table with column names and units
+
+Notes
+-----
+
+"""
+
+GCData = namedtuple('GCData', ['provenance', 'table'])
