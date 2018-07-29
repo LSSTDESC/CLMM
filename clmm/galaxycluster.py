7fc830de70c601256de81e10a09cb762783a93cf
m-aguena <aguena@if.usp.br>
2018-07-29 16:25:19 -0700

    move auxiliary functions to AuxFuncs class

--- clmm/galaxycluster.py
+++ clmm/galaxycluster.py
@@ -49,137 +49,8 @@ class GalaxyCluster():
 
         return test_creator in self.data
 
-    def _check_subdict(self, test_dict, main_dict):
-        '''
-        Checks if a dictionary is a subset of another dictionary, with the same keys and values
-
-        Parameters
-        ----------
-        test_dict: dict
-            subset dictionary
-        main_dict: dict
-            main dictionary
-
-        Returns
-        -------
-        bool
-            if a test_dict is a subset of main_dict
-        '''
-
-        for test_key, test_value in test_dict.items():
-
-            if test_key in main_dict:
-
-                if test_value != main_dict[test_key]:
-
-                        return False
-
-            else:
-
-                return False
-
-        return True
 
 
-    def _find_in_datalist(self, datalist, lookup_specs):
-        '''
-        Finds data with given specs in a datalist,
-            allows for partial match
-
-        Parameters
-        ----------
-        datalist: list
-            list of clmm.GCData objects to search for lookup_specs
-        lookup_specs: dict
-            specs required
-
-        Returns
-        -------
-        list, None
-            list of clmm.GCData object data with required creator and set of specs
-            if not objects are found, returns None
-        '''
-
-        found = []
-
-        for data in datalist:
-
-            if self._check_subdict(lookup_specs, data.specs) :
-
-                found.append( data )
-
-        if len(found) == 0:
-
-            print('*** WARNING *** no data found with these specification!')
-            found = None
-
-        elif len(found) > 1:
-
-            print('*** WARNING *** multiple data found with these specification!')
-            
-        return found
-
-    def _find_ind_in_datalist_exact(self, datalist, lookup_specs):
-        '''
-        Finds data with given specs in a datalist,
-            requires exact match
-
-        Parameters
-        ----------
-        datalist: list
-            list of clmm.GCData objects to search for lookup_specs
-        lookup_specs: dict
-            specs required
-
-        Returns
-        -------
-        int, None
-            index of clmm.GCData object in datalist with required creator and set of specs
-            if not found, returns None
-
-        '''
-
-        for ind, data in enumerate(datalist):
-
-            if lookup_specs == data.specs :
-
-                return ind
-
-        else:
-
-            print('*** ERROR *** - no data found with these specification!')
-            return None
-    
-    def _find_in_datalist_exact(self, datalist, lookup_specs):
-        '''
-        Finds data with given specs in a datalist,
-            requires exact match
-
-        Parameters
-        ----------
-        datalist: list
-            list of clmm.GCData objects to search for lookup_specs
-        lookup_specs: dict
-            specs required
-
-        Returns
-        -------
-        list
-            list of clmm.GCData object data with required creator and set of specs
-            if not found, returns None
-
-        '''
-
-        for data in datalist:
-
-            if lookup_specs == data.specs :
-
-                return data
-
-        else:
-
-            print('*** ERROR *** - no data found with these specification!')
-            return None
 
     def _find(self, lookup_creator, lookup_specs):
         '''
@@ -202,7 +73,7 @@ class GalaxyCluster():
 
         if self._check_creator(lookup_creator):
 
-            return self._find_in_datalist(self.data[lookup_creator], lookup_specs)
+            return AuxFuncs._find_in_datalist(self.data[lookup_creator], lookup_specs)
 
         else:
 
@@ -230,7 +101,7 @@ class GalaxyCluster():
 
         if self._check_creator(lookup_creator):
 
-            return self._find_in_datalist_exact(self.data[lookup_creator], lookup_specs)
+            return AuxFuncs._find_in_datalist_exact(self.data[lookup_creator], lookup_specs)
 
         else:
 
@@ -271,14 +142,14 @@ class GalaxyCluster():
 
         if self._check_creator(incoming_data.creator):
 
-            found_data = self._find_in_datalist_exact(self.data[incoming_data.creator], incoming_data.specs)
+            found_data = AuxFuncs._find_in_datalist_exact(self.data[incoming_data.creator], incoming_data.specs)
 
             if found_data:
 
                 if replace:
 
                     print('Overwritting this data:')
-                    self._remove_in_datalist(self.data[incoming_data.creator], incoming_data.specs)
+                    AuxFuncs._remove_in_datalist(self.data[incoming_data.creator], incoming_data.specs)
                     self.data[incoming_data.creator].append( incoming_data )
 
                 else:
@@ -296,23 +167,157 @@ class GalaxyCluster():
 
             self.data[incoming_data.creator] = [ incoming_data ]
 
-    def _remove_in_datalist(self, datalist, specs):
-
-        ind = self._find_ind_in_datalist_exact(datalist, specs)
-
-        if ind is not None:
-
-            del datalist[ind]
-
     def _remove(self, creator, specs):
 
         if self._check_creator(creator):
 
-            if self._find_in_datalist_exact(self.data[creator], specs):
+            if AuxFuncs._find_in_datalist_exact(self.data[creator], specs):
 
-                self._remove_in_datalist(self.data[creator], specs)
+                AuxFuncs._remove_in_datalist(self.data[creator], specs)
 
         else:
 
             print('*** ERROR *** - creator "%s" not found in datalist'%creator)
+
+
+class AuxFuncs():
+
+    def _check_subdict(test_dict, main_dict):
+        '''
+        Checks if a dictionary is a subset of another dictionary, with the same keys and values
+
+        Parameters
+        ----------
+        test_dict: dict
+            subset dictionary
+        main_dict: dict
+            main dictionary
+
+        Returns
+        -------
+        bool
+            if a test_dict is a subset of main_dict
+        '''
+
+        for test_key, test_value in test_dict.items():
+
+            if test_key in main_dict:
+
+                if test_value != main_dict[test_key]:
+
+                        return False
+
+            else:
+
+                return False
+
+        return True
+
+    def _find_in_datalist(datalist, lookup_specs):
+        '''
+        Finds data with given specs in a datalist,
+            allows for partial match
+
+        Parameters
+        ----------
+        datalist: list
+            list of clmm.GCData objects to search for lookup_specs
+        lookup_specs: dict
+            specs required
+
+        Returns
+        -------
+        list, None
+            list of clmm.GCData object data with required creator and set of specs
+            if not objects are found, returns None
+        '''
+
+        found = []
+
+        for data in datalist:
+
+            if AuxFuncs._check_subdict(lookup_specs, data.specs) :
+
+                found.append( data )
+
+        if len(found) == 0:
+
+            print('*** WARNING *** no data found with these specification!')
+            found = None
+
+        elif len(found) > 1:
+
+            print('*** WARNING *** multiple data found with these specification!')
+            
+        return found
+
+    def _find_ind_in_datalist_exact(datalist, lookup_specs):
+        '''
+        Finds data with given specs in a datalist,
+            requires exact match
+
+        Parameters
+        ----------
+        datalist: list
+            list of clmm.GCData objects to search for lookup_specs
+        lookup_specs: dict
+            specs required
+
+        Returns
+        -------
+        int, None
+            index of clmm.GCData object in datalist with required creator and set of specs
+            if not found, returns None
+
+        '''
+
+        for ind, data in enumerate(datalist):
+
+            if lookup_specs == data.specs :
+
+                return ind
+
+        else:
+
+            print('*** ERROR *** - no data found with these specification!')
+            return None
+    
+    def _find_in_datalist_exact(datalist, lookup_specs):
+        '''
+        Finds data with given specs in a datalist,
+            requires exact match
+
+        Parameters
+        ----------
+        datalist: list
+            list of clmm.GCData objects to search for lookup_specs
+        lookup_specs: dict
+            specs required
+
+        Returns
+        -------
+        list
+            list of clmm.GCData object data with required creator and set of specs
+            if not found, returns None
+
+        '''
+
+        for data in datalist:
+
+            if lookup_specs == data.specs :
+
+                return data
+
+        else:
+
+            print('*** ERROR *** - no data found with these specification!')
+            return None
+
+    def _remove_in_datalist(datalist, specs):
+
+        ind = AuxFuncs._find_ind_in_datalist_exact(datalist, specs)
+
+        if ind is not None:
+
+            del datalist[ind]
 
