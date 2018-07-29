681049223fcf4dd050c5a22075d862aaab46daaa
m-aguena <aguena@if.usp.br>
2018-07-29 13:27:54 -0700

    Âadd middle changes

--- clmm/galaxycluster.py
+++ clmm/galaxycluster.py
@@ -7,33 +7,215 @@ Who to blame for problems: Michel A.
 class GalaxyCluster():
     '''
     Object that contains the properties of a galaxy cluster.
-
-    Parameters
-    ----------
-
-        data: dictionary with all cluster properties.
-            Each entry  must contain 
-        homelocal: path to save cluster properties
-
-
-    It must have this opperations:
-        - read data
-        - write data
-        - disting data
     '''
 
     def __init__(self, initial_data=None, homelocal='.'):
         '''
+        Parameters
+        ----------
+        data: list of clmm.GCData objects, optional
+            initial data to associate with GalaxyCluster object
+        homelocal: string, optionnal
+            path to save cluster properties
+
         Notes
         -----
 
         '''
-        self.data = []
+        self.data = {}
         self.homelocal = homelocal
-        self.creators = {}
+        self._debugprint = False
 
         if initial_data is not None:
             self._add(initial_data)
+    
+    def _check_creator(self, test_creator):
+        '''
+        Checks if a creator exists in GalaxyCluster object
+
+        Parameters
+        ----------
+        test_creator: string
+            creator that will be searched in GalaxyCluster object
+
+        Returns
+        -------
+        bool
+            existence of the test_creator in GalaxyCluster object
+        '''
+
+        return test_creator in self.data
+
+    def _check_subdict(self, test_dict, main_dict):
+        '''
+        Checks if a dictionary is a subset of another dictionary, with the same keys and values
+
+        Parameters
+        ----------
+        test_dict: dict
+            subset dictionary
+        main_dict: dict
+            main dictionary
+
+        Returns
+        -------
+        bool
+            if a test_dict is a subset of main_dict
+        '''
+
+        for test_key, test_value in test_dict:
+
+            if test_key in main_dict:
+
+                if test_value != main_dict[test_key]:
+
+                        return False
+
+            else:
+
+                return False
+
+        return True
+
+    def _find_in_datalist(self, datalist, lookup_specs):
+        '''
+        Finds data with given specs in a datalist,
+            allows for partial match
+
+        Parameters
+        ----------
+        datalist: list
+            list of clmm.GCData objects to search for lookup_specs
+        lookup_specs: dict
+            specs required
+
+        Returns
+        -------
+        list, False
+            list of clmm.GCData object data with required creator and set of specs
+            if not objects are found, returns False
+        '''
+
+        found = []
+
+        for data in datalist:
+
+            if self._check_subdict(lookup_specs, data.specs) :
+
+                found.append( data )
+
+        if len(found) == 0:
+
+            print('WARNING, no data found with these specification!')
+            found = False
+
+        if len(found) > 1:
+
+            print('WARNING, multiple data found with these specification!')
+            
+        return found
+    
+    def _find_in_datalist_exact(self, datalist, lookup_specs):
+        '''
+        Finds data with given specs in a datalist,
+            requires exact match
+
+        Parameters
+        ----------
+        datalist: list
+            list of clmm.GCData objects to search for lookup_specs
+        lookup_specs: dict
+            specs required
+
+        Returns
+        -------
+        list
+            list of clmm.GCData object data with required creator and set of specs
+            if not found, returns False
+
+        '''
+
+        for data in self.data[lookup_creator]:
+
+            if lookup_specs == data.specs :
+
+                return data
+
+        else:
+
+            print('ERROR, no data found with these specification!')
+            return False
+
+    def _find(self, lookup_creator, lookup_specs):
+        '''
+        Finds data with a specific cretor and specs in GalaxyCluster object
+            allows for partial match
+
+        Parameters
+        ----------
+        creator: string
+            creator that will be searched in GalaxyCluster object
+        specs: dict
+            specs requiered inside the creator
+
+        Returns
+        -------
+        list, False
+            list of clmm.GCData object data with required creator and set of specs
+            if not objects are found, returns False
+        '''
+
+        if self._check_creator(lookup_creator):
+
+            for data in self.data[lookup_creator]:
+
+                if self._check_subdict(lookup_specs, data.specs) :
+
+                    found.append( data )
+
+        if len(found) == 0:
+
+            print('WARNING, no data found with these specification!')
+            found = False
+
+        if len(found) > 1:
+
+            print('WARNING, multiple data found with these specification!')
+            
+        return found
+    
+    def _find_exact(self, lookup_creator, lookup_specs):
+        '''
+        Finds data with a specsific cretor and specs in GalaxyCluster object
+            requires exact match
+
+        Parameters
+        ----------
+        creator: string
+            creator that will be searched in GalaxyCluster object
+        specs: dict
+            specs requiered inside the creator
+
+        Returns
+        -------
+        list
+            list of clmm.GCData object data with required creator and set of specs
+            if not found, returns False
+
+        '''
+
+        if self._check_creator(lookup_creator):
+
+            for data in self.data[lookup_creator]:
+
+                if lookup_specs == data.specs :
+
+                    return data
+
+        else:
+
+            print('ERROR, no data found with these specification!')
+            return False
 
     def _add(self, incoming_data, replace=False):
         '''
@@ -43,25 +225,21 @@ class GalaxyCluster():
             new data to associate with GalaxyCluster object
         incoming_metadata: dict
             new metadata for GalaxyCluster to use to distinguish from other clmm.GCData with same provenance
+        replace: bool, optional
+            replace in the case of data with same creator, specs already exists
        
         Notes
         -----
 
          '''
 
-        if incoming_metadata.creator in self.creators:
+        if self._check_creator(incoming_data.creator):
 
-            self.creators[incoming_data.creator].append( len(self.data) )
+            if self._check_specs_exact(incoming_data.creator, incoming_data.specs):
 
-            for i in self.creators[incoming_metadata.creator]:
-
-                if incoming_metadata.specs = self.data[i].specs:
-
-                    if not replace:
-
-                        print('Data "%s" already exists, to replace it, add replace=True key.')
-                        print('Current:')
-                        self._show(metadata)
+                print('Data with this creator, specs already exists, to replace it, add replace=True key.')
+                print('Current:')
+                self._show(metadata)
                         return 0
 
                     else:
@@ -71,37 +249,8 @@ class GalaxyCluster():
 
         else:
 
-            self.creators[incoming_data.creator] = [ len(self.data) ]
+            self.data[incoming_data.creator].append( incomming_data )
 
-        self.data.append( incomming_data )
-
-    def _find(self, input_creator, input_metadata):
-        '''
-        still in construction, must figure out what to do if self.data has more than one data
-            with the required creator and specs
-        '''
-
-        right_i = None
-
-        if input_creator in self.creators:
-
-            for i in self.creators[input_creator]:
-
-                right_i  = i
-
-                for key, value in input_metadata:
-
-                    if key in self.data[i].specs:
-
-                        if value != self.data[i].specs[key]
-
-                            right_i = None
-                    else:
-
-                        print('ERROR, key "%s" does not exist in %s'%(key, input_creator))
-                        return 0
-    
-            return self.data[right_i]
 
     def _listprofiles():
         '''
