474e0ff2640f913198f9e5c19d68d3688ba56d93
matthewkirby <matthewkirby@email.arizona.edu>
2018-07-29 18:20:01 -0400

    First pass at models.Model tests

--- clmm/tests/test_model.py
+++ clmm/tests/test_model.py
@@ -1,4 +1,36 @@
 import models.models as models
+from numpy.testing import assert_raises
 
-def test_models() :
-    pass
+
+def assert_block(test_model):
+    assert(callable(test_model.func))
+
+    assert(isinstance(test_model.independent_vars, list) or (test_model.independent_vars is None))
+    if test_model.independent_vars is not None:
+        for element in test_model.independent_vars:
+            assert(isinstance(element, str))
+
+    assert(isinstance(test_model.params, list) or (test_model.params is None))
+    if test_model.params is not None:
+        for element in test_model.params:
+            assert(isinstance(element, Parameter))
+
+
+def test_model_superclass() :
+    # model.Models (callable [func], list of str, [independent_vars], list of
+    # Parameter obj [params])
+
+    assert_raises(TypeError, models.Model, lambda x:x, ['r'], [P])
+    assert_raises(TypeError, models.Model, lambda x:x, ['r'], ['param1'])
+    assert_raises(TypeError, models.Model, lambda x:x, [1])
+    assert_raises(TypeError, models.Model, lambda x:x, 'r')
+    assert_raises(TypeError, models.Model, 'x*x', ['r'])
+
+    test_model = models.Model(lambda x:x)
+    assert_block(test_model)
+   
+    test_model = models.Model(lambda x:x, ['r'])
+    assert_block(test_model)
+
+    test_model = models.Model(lambda x: x*x, ['r'], [Parameter()])
+    assert_block(test_model)
