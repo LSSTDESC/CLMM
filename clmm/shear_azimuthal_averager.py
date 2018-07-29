72da799536d7522ab1a0e5ee0128cc27affa9757
Hao-Yi Wu <haoyiwu@edison03.nersc.gov>
2018-07-29 09:25:04 -0700

    change the input to astorpy table for sources and dict for clusters

--- clmm/shear_azimuthal_averager.py
+++ clmm/shear_azimuthal_averager.py
@@ -2,29 +2,59 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy.cosmology import FlatLambdaCDM
-#import FoFCatalogMatching
-#import GCRCatalogs
-
-
-# see shear_azimuthal_averager_demo.py for demo
 
 class ShearAzimuthalAverager(object):
-    def __init__(self, ra_src, dec_src, z_src, ra_cl, dec_cl, z_cl, gamma_1, gamma_2):
-        # TODO: pass catalog table directly!
+    """
+    a class that calculate the average azimuthal profile for shear
+    see shear_azimuthal_averager_demo.py for demo
+
+    Attributes
+    ----------
+
+    """
+    def __init__(self, cl_dict, src_table):
+        """
+        Parameters
+        ----------
+        cl_dict : dictionary
+            containing the infomration of cluster
+
+        st_table: astropy table
+            containing the infomration of sources
+
+        """
         # TODO: check whether property exists in the catalog table
-        self.ra_src = ra_src
-        self.dec_src = dec_src
-        self.z_src = z_src
-        self.ra_cl = ra_cl
-        self.dec_cl = dec_cl
-        self.z_cl = z_cl
-        self.gamma_1 = gamma_1
-        self.gamma_2 = gamma_2
+        
+
+        # cluster information
+        self.ra_cl = cl_dict['ra']
+        self.dec_cl = cl_dict['dec']
+        self.z_cl = cl_dict['z']
+
+        # source infomration
+        # first, check whether it has convergence
+        self.id_src = src_table['id']
+        self.ra_src = src_table['ra']
+        self.dec_src = src_table['dec']
+        self.z_src = src_table['z']
+
+        if 'kappa' in src_table.keys():
+            print('this is sim!')
+            self.gamma_1 = src_table['gamma1']
+            self.gamma_2 = src_table['gamma2']
+            self.kappa = src_table['kappa']
+            self.g1 = self.gamma_1/(1-self.kappa)
+            self.g2 = self.gamma_2/(1-self.kappa)
+
+        else:
+            print('read g1, g2 directly')
+
+
+        # source information
         self.cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
 
 
     def read_catalog(self):
-        pass# done outside!
         """ 
         This reads in the galaxy catalog, which can either:
         - DC2 extragalactic catalog (shear1, shear2)
@@ -36,7 +66,8 @@ class ShearAzimuthalAverager(object):
         - Homogenised astropy table containing at least: ra[deg], dec[deg], e1, e2, (z or p(z)), relevant flags...
      
         """
-        
+        pass# done outside!
+
     def compute_shear(self):
         """
         Input: galaxy catalog astropy table
@@ -50,12 +81,12 @@ class ShearAzimuthalAverager(object):
         gamc =  e1 * numpy.sin(2.0 * phi) - e2 * numpy.cos(2.0 * phi)
         dist = fcn(cosmology, phi, z)
         """
-        shear1 = self.gamma_1
-        shear2 = self.gamma_2
+        shear1 = self.g1
+        shear2 = self.g2
         phi = np.arctan2(self.dec_src-self.dec_cl, self.ra_src-self.ra_cl)
-        self.gamt = - (shear1 * np.cos(2.0 * phi) - shear2 * np.sin(2.0 * phi))
-        self.gamc = - shear1 * np.sin(2.0 * phi) + shear2 * np.cos(2.0 * phi)
-        #plt.hist(self.gamt)
+        self.shear_t = - (shear1 * np.cos(2.0 * phi) - shear2 * np.sin(2.0 * phi))
+        self.shear_c = - shear1 * np.sin(2.0 * phi) + shear2 * np.cos(2.0 * phi)
+        #plt.hist(self.shear_t)
 
 
     def make_bins(self, user_defined=False):
@@ -103,21 +134,21 @@ class ShearAzimuthalAverager(object):
             r_max = r_arr[ir+1]
             select = (phys_dist >= r_min) & (phys_dist < r_max)
             rmean_arr[ir] = np.mean(phys_dist[select])
-            print(np.mean(self.gamt[select]), len(self.gamt[select]))
-            gamt_arr[ir] = np.mean(self.gamt[select])
-            gamt_err_arr[ir] = np.std(self.gamt[select])
-            gamc_arr[ir] = np.mean(self.gamc[select])
-            gamc_err_arr[ir] = np.std(self.gamc[select])
+            print(np.mean(self.shear_t[select]), len(self.shear_t[select]))
+            gamt_arr[ir] = np.mean(self.shear_t[select])
+            gamt_err_arr[ir] = np.std(self.shear_t[select])
+            gamc_arr[ir] = np.mean(self.shear_c[select])
+            gamc_err_arr[ir] = np.std(self.shear_c[select])
 
         self.rmean_arr = rmean_arr
-        self.gamt_arr = gamt_arr
-        self.gamc_arr = gamc_arr
-        self.gamt_err_arr = gamt_err_arr
-        self.gamc_err_arr = gamc_err_arr
+        self.shear_t_arr = gamt_arr
+        self.shear_c_arr = gamc_arr
+        self.shear_t_err_arr = gamt_err_arr
+        self.shear_c_err_arr = gamc_err_arr
 
     def plot_profile(self):
-        plt.errorbar(self.rmean_arr, self.gamt_arr, yerr=self.gamt_err_arr, label=r'$\gamma_t$')
-        plt.errorbar(self.rmean_arr, self.gamc_arr, yerr=self.gamc_err_arr, label=r'$\gamma_\times$')
+        plt.errorbar(self.rmean_arr, self.shear_t_arr, yerr=self.shear_t_err_arr, label=r'$g_t$')
+        plt.errorbar(self.rmean_arr, self.shear_c_arr, yerr=self.shear_c_err_arr, label=r'$g_\times$')
         #plt.xscale('log')
         #plt.yscale('log')
         plt.xlabel('physical Mpc')
@@ -125,4 +156,49 @@ class ShearAzimuthalAverager(object):
         plt.savefig('plots/profile_test.pdf')
 
 
+if __name__ == "__main__":
+    import GCRCatalogs
+    from astropy.table import Table
+
+    extragalactic_cat = GCRCatalogs.load_catalog('proto-dc2_v2.1.2_test')
+    # get a massive halo!
+    massive_halos = extragalactic_cat.get_quantities(['halo_mass', 'redshift','ra', 'dec'], filters=['halo_mass > 1e14','is_central==True'])
+
+    m = massive_halos['halo_mass']
+    select = (m == np.max(m))
+    ra_cl = massive_halos['ra'][select][0]
+    dec_cl = massive_halos['dec'][select][0]
+    z_cl = massive_halos['redshift'][select][0]
+
+    # make a dictionary for clusterr
+    cl_dict = {'z':z_cl, 'ra':ra_cl, 'dec': dec_cl}
+
+    # make a astropy table for source
+
+    # get galaxies around it
+    ra_min, ra_max = ra_cl-0.3, ra_cl+0.3
+    dec_min, dec_max = dec_cl-0.3, dec_cl+0.3
+    z_min = z_cl + 0.1
+    z_max = 1.5
+
+    coord_filters = [
+        'ra >= {}'.format(ra_min),
+        'ra < {}'.format(ra_max),
+        'dec >= {}'.format(dec_min),
+        'dec < {}'.format(dec_max),
+    ]
+    z_filters = ['redshift >= {}'.format(z_min),'redshift < {}'.format(z_max)]
+    gal_cat = extragalactic_cat.get_quantities(['ra', 'dec', 'shear_1', 'shear_2', 'shear_2_phosim', 'shear_2_treecorr','redshift','galaxy_id','convergence'], filters=(coord_filters + z_filters))
+
+    t = Table([gal_cat['galaxy_id'],gal_cat['ra'],gal_cat['dec'],gal_cat['shear_1'],\
+          gal_cat['shear_2'],gal_cat['redshift'],gal_cat['convergence']], \
+          names=('id','ra','dec', 'gamma1', 'gamma2', 'z', 'kappa'))
+
+    # crerate an object
+    saa = ShearAzimuthalAverager(cl_dict=cl_dict, src_table=t)
+    saa.compute_shear()
+    saa.make_shear_profile()
+    saa.plot_profile()
+
+    plt.show()
    
