0c03adeb34c04a1975517eccd585b9d50612d9fc
matthewkirby <matthewkirby@email.arizona.edu>
2018-07-30 19:20:58 -0400

    Put the correct license in setuppy

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
       include_package_data=True,
       classifiers=[
         "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: BSD 3-Clause",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "Operating System :: OS Independent",
