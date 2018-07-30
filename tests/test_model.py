cd846aea197e61321d92f34512a410ba9f4e7934
Alex Malz <aimalz@nyu.edu>
2018-07-30 17:41:08 -0400

    updated test for new names

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,4 +1,4 @@
-"""Tests for the models.models modules"""
+"""Tests for the models.model module"""
 from __future__ import absolute_import, print_function
 
 import numpy as np
@@ -6,12 +6,13 @@ from numpy.testing import assert_raises
 import six
 
 import clmm
-from clmm.models import models
+from clmm import models
+from clmm.models.model import Model
 from clmm.models.parameter import Parameter
 
 
 def assert_block(test_model):
-    """Block of asserts for type checks in models.Model
+    """Block of asserts for type checks in models.model
 
     Parameters
     ----------
@@ -37,18 +38,18 @@ def assert_block(test_model):
 
 
 def test_model_superclass():
-    """Test the Models superclass. """
+    """Test the Model superclass. """
 
-    assert_raises(TypeError, models.Model, lambda x: x, [1])
-    assert_raises(TypeError, models.Model, lambda x: x, 'r')
-    assert_raises(TypeError, models.Model, 'x*x', ['r'])
-    assert_raises(TypeError, models.Model, lambda x: x, ['r'], ['param1'])
+    assert_raises(TypeError, Model, lambda x: x, [1])
+    assert_raises(TypeError, Model, lambda x: x, 'r')
+    assert_raises(TypeError, Model, 'x*x', ['r'])
+    assert_raises(TypeError, Model, lambda x: x, ['r'], ['param1'])
 
-    test_model = models.Model(lambda x: x)
+    test_model = Model(lambda x: x)
     assert_block(test_model)
 
-    test_model = models.Model(lambda x: x, ['r'])
+    test_model = Model(lambda x: x, ['r'])
     assert_block(test_model)
 
-    test_model = models.Model(lambda x: x*x, ['r'], [Parameter()])
+    test_model = Model(lambda x: x*x, ['r'], [Parameter()])
     assert_block(test_model)
