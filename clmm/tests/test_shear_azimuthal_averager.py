bec2c717fb6a202607e07d3c7da5c32aa7e853e5
Celine Combet <ccombet@cori19.nersc.gov>
2018-07-30 13:19:41 -0700

    Fix test import

--- clmm/tests/test_shear_azimuthal_averager.py
+++ clmm/tests/test_shear_azimuthal_averager.py
@@ -1,57 +1,55 @@
 #!/usr/bin/env python
-#import matplotlib.pyplot as plt
 import numpy as np
 from astropy.table import Table
+# using DC2
+import GCRCatalogs
 # model from Dallas group
 import colossus.cosmology.cosmology as Cosmology # used for distances
-import CLMM_densityModels_beforeConvertFromPerH as clmm
+import models.CLMM_densityModels_beforeConvertFromPerH as clmm
 # the code we want to test:
-from shear_azimuthal_averager import ShearAzimuthalAverager
-
+from summarizer.shear_azimuthal_averager import ShearAzimuthalAverager
 
 
 def test_shear_azimuthal_averager():
-    # compute the profile using our code
+    extragalactic_cat = GCRCatalogs.load_catalog('proto-dc2_v2.1.2_test')
 
-    # take an ideal catalog made by Celine
-    cl_dict = {'z':0.3, 'ra':0, 'dec': 0, 'm':1e15, 'c':4}
+    # get a massive halo!
+    massive_halos = extragalactic_cat.get_quantities(['halo_mass', 'redshift','ra', 'dec'],\
+                                                     filters=['halo_mass > 1e14','is_central==True'])
 
-    filename='/global/u1/c/ccombet/shearfile.hdf5'
+    m = massive_halos['halo_mass']
+    select = (m == np.max(m))
+    ra_cl = massive_halos['ra'][select][0]
+    dec_cl = massive_halos['dec'][select][0]
+    z_cl = massive_halos['redshift'][select][0]
+    m_cl = m[select][0]
+    print(m[select], ra_cl, dec_cl, z_cl)
 
-    atab = Table.read(filename, path='deepCoadd_meas')
-    ztab = Table.read(filename, path='zphot_ref')
-    t = Table([atab['id'],atab['coord_ra_deg'],atab['coord_dec_deg'],
-                atab['ext_shapeHSM_HsmShapeRegauss_e1'],atab['ext_shapeHSM_HsmShapeRegauss_e2'],
-                ztab['Z_BEST'],atab['id']*0], names=('id','ra','dec', 'gamma1', 'gamma2', 'z', 'kappa'))
-
-    # # plt.scatter(new_tab['ra'], new_tab['dec'], s=2)
-    # # plt.show()
-    # saa = ShearAzimuthalAverager(cl_dict=cl_dict,src_table=new_tab)
-    # saa.compute_shear()
-    # binned_profile = saa.make_shear_profile()
+    # make a dictionary for cluster
+    cl_dict = {'z':z_cl, 'ra':ra_cl, 'dec': dec_cl}
 
 
-    # # get the galaxies around it
-    # ra_min, ra_max = ra_cl-0.3, ra_cl+0.3
-    # dec_min, dec_max = dec_cl-0.3, dec_cl+0.3
-    # z_min = z_cl + 0.1
-    # z_max = 1.5
+    # get the galaxies around it
+    ra_min, ra_max = ra_cl-0.3, ra_cl+0.3
+    dec_min, dec_max = dec_cl-0.3, dec_cl+0.3
+    z_min = z_cl + 0.1
+    z_max = 1.5
 
-    # coord_filters = [
-    #     'ra >= {}'.format(ra_min),
-    #     'ra < {}'.format(ra_max),
-    #     'dec >= {}'.format(dec_min),
-    #     'dec < {}'.format(dec_max),
-    # ]
-    # z_filters = ['redshift >= {}'.format(z_min),'redshift < {}'.format(z_max)]
+    coord_filters = [
+        'ra >= {}'.format(ra_min),
+        'ra < {}'.format(ra_max),
+        'dec >= {}'.format(dec_min),
+        'dec < {}'.format(dec_max),
+    ]
+    z_filters = ['redshift >= {}'.format(z_min),'redshift < {}'.format(z_max)]
 
-    # gal_cat = extragalactic_cat.get_quantities(['galaxy_id', 'ra', 'dec', 'shear_1', 'shear_2', 'shear_2_phosim', 'shear_2_treecorr',\
-    #                                             'redshift', 'convergence'], filters=(coord_filters + z_filters))
+    gal_cat = extragalactic_cat.get_quantities(['galaxy_id', 'ra', 'dec', 'shear_1', 'shear_2', 'shear_2_phosim', 'shear_2_treecorr',\
+                                                'redshift', 'convergence'], filters=(coord_filters + z_filters))
 
-    # # store the results into an astropy table
-    # t = Table([gal_cat['galaxy_id'],gal_cat['ra'],gal_cat['dec'],gal_cat['shear_1'],\
-    #           gal_cat['shear_2'],gal_cat['redshift'],gal_cat['convergence']], \
-    #           names=('id','ra','dec', 'gamma1', 'gamma2', 'z', 'kappa'))
+    # store the results into an astropy table
+    t = Table([gal_cat['galaxy_id'],gal_cat['ra'],gal_cat['dec'],gal_cat['shear_1'],\
+              gal_cat['shear_2'],gal_cat['redshift'],gal_cat['convergence']], \
+              names=('id','ra','dec', 'gamma1', 'gamma2', 'z', 'kappa'))
 
 
     # create an object, given cluster dictionary and galaxy astropy table
@@ -66,13 +64,13 @@ def test_shear_azimuthal_averager():
 
 
     # set up a model
-    zL = cl_dict['z'] # NOTE!
+    zL = z_cl # NOTE!
     chooseCosmology = 'WMAP7-ML' #Choose cosmology used
     cosmo = Cosmology.setCosmology(chooseCosmology)
 
     mdef = '200c'
-    M = cl_dict['m']*cosmo.h
-    c = cl_dict['c']
+    M = m_cl*cosmo.h # NOTE! model code operate with h
+    c = 4. # NOTE! n=not sure
     r = np.linspace(0.5, 3., 100) #Mpc
     r = r*cosmo.h #Mpc/h
 
@@ -108,17 +106,10 @@ def test_shear_azimuthal_averager():
     print(g_t_residual)
     print(g_x_residual)
 
-    tolerance = 0.1 # 
+    tolerance = 1 # 
     assert g_t_residual < tolerance
     assert g_x_residual < tolerance
 
-    # plotting = True
-    # if plotting==True:
-    #     plt.plot(r_phys, gt_mod, label='model')
-    #     plt.errorbar(r_phys, binned_profile['g_t'], yerr=binned_profile['g_t_err'], label='data')
-    #     plt.legend()
-    #     plt.show()
-
-
+    
 
 test_shear_azimuthal_averager()
