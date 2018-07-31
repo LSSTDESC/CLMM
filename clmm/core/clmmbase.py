e2f9deb20a3cb465a7c6b34b40296134dc44eb6f
Matthew Kirby <matthewkirby@email.arizona.edu>
2018-07-31 15:30:38 -0700

    Made the ultra super class, wrote tests for it and all ancestors.

--- clmm/core/clmmbase.py
+++ clmm/core/clmmbase.py
@@ -1,4 +1,15 @@
 """Define a superclass that all classes in the CLMM package inherit from"""
+import numpy as np
 
-class __CLMMBase():
-    _ask_type = ''
+class CLMMBase():
+    _ask_type = []
+
+    @property
+    def ask_type(self):
+        return self._ask_type
+
+    @ask_type.setter
+    def ask_type(self, ask_type_list):
+        if not np.iterable(ask_type_list):
+            raise TypeError()
+        self._ask_type = ask_type_list
