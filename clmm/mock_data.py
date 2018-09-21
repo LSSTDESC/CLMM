d15a85f2782b7d1b52c1e88affb0ea48ec2b0e26
Celine Combet <ccombet@cori19.nersc.gov>
2018-09-21 06:03:45 -0700

    Start mock data module and demo

---
+++ clmm/mock_data.py
@@ -0,0 +1,118 @@
+import sys
+from clmm.models import CLMM_densityModels_beforeConvertFromPerH as clmm_mod
+import numpy as np
+import colossus.cosmology.cosmology as Cosmology
+import matplotlib.pyplot as plt
+from astropy.table import Table
+
+
+class MockData(): 
+    """
+    a class that generates a mock galaxy catalog around a galaxy cluster
+    
+    Attributes
+    ----------
+    config: dictionary
+        The configuration to generate the fake data. The cluster is located in (0,0).
+        The fields of the dictionary should be:
+        ngals: int
+            number of galaxies in the fake catalog
+        cluster_m: float
+            mass of the cluster
+        cluster_z: float
+            redshift of cluster
+        concentration: float
+            concentration of the cluster
+        src_z: float
+            redshift of background galaxies
+        cosmo: string 
+            Defines the cosmological parameter set in colossus, e.g. WMAP7-ML
+        mdef: string
+            Mass definition, e.g. '200c'
+            
+    catalog: astropy table
+        The catalog generated given the user-defined configuration
+
+    """
+
+    
+    def __init__(self, config=None):
+        """
+        Parameters
+        ----------
+
+        """
+        if config is not None:
+            self.config = config
+        else:
+            self.config = {}
+            self.config['ngals'] = int(3.e4)
+            self.config['cluster_m'] = 1.e15
+            self.config['cluster_z'] = 0.3
+            self.config['src_z'] = 0.8
+            self.config['cosmo'] = 'WMAP7-ML'
+            self.config['mdef'] = '200c'
+            self.config['concentration'] = 4
+            
+
+    def generate(self,is_shape_noise=False, is_z_err=False):
+        '''
+        This generates a fake dataset of background galaxies using the Dallas group software. 
+        By default, data are perfect, i.e. no shape noise, all galaxies at the same redshift.
+        TODO: implement the shape noise, and redshift error options
+
+        Options:
+        --------
+        is_shape_noise: Boolean
+            If True, noise is added to the galaxy shapes
+        is_z_err: Boolean
+            If True, a photometric redshift error is added to the background galaxy redshifts
+        
+        Returns:
+        ---------
+            astropy table containing the galaxy catalog [id, ra, dec, gamma1, gamma2, z]
+        '''
+
+        if is_shape_noise:
+            self.config['shape_noise'] = is_shape_noise
+
+        if is_z_err:
+            self.config['redshift_err'] = is_z_noise
+
+        if is_shape_noise or is_z_err:
+            raise ValueError('Options not implemented yet')
+
+        ngals = self.config['ngals']
+        seqnr = np.arange(ngals)
+        zL = self.config['cluster_z'] # cluster redshift
+        mdef = self.config['mdef']
+        cosmo = Cosmology.setCosmology(self.config['cosmo'])
+
+        M = self.config['cluster_m']*cosmo.h
+        c = self.config['concentration']  
+        r = np.linspace(0.25, 10., 1000) #Mpc
+        r = r*cosmo.h #Mpc/h
+
+        testProf = clmm_mod.nfwProfile(M = M, c = c, zL = zL, mdef = mdef, \
+                                chooseCosmology = self.config['cosmo'], esp = None)
+
+        x_mpc = np.random.uniform(-4, 4, size=ngals)
+        y_mpc = np.random.uniform(-4, 4, size=ngals)
+        r_mpc = np.sqrt(x_mpc**2 + y_mpc**2)
+
+        Dl = cosmo.angularDiameterDistance(zL)
+
+        x_deg = (x_mpc/Dl)*(180./np.pi) #ra
+        y_deg = (y_mpc/Dl)*(180./np.pi) #dec
+
+        gamt= testProf.deltaSigma(r_mpc)/testProf.Sc(self.config['src_z'])
+
+        posangle = np.arctan2(y_mpc, x_mpc)
+        cos2phi = np.cos(2*posangle)
+        sin2phi = np.sin(2*posangle)
+
+        e1 = -gamt*cos2phi
+        e2 = -gamt*sin2phi
+
+        self.catalog = Table([seqnr, -x_deg, y_deg, e1, e2, np.zeros(ngals)+self.config['src_z']], \
+                            names=('id', 'ra','dec','gamma1','gamma2', 'z'))
