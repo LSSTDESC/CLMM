dd38541d2c457043a6691497014e26000eae8d81
Celine Combet <celine.combet@lpsc.in2p3.fr>
2018-10-03 05:48:19 -0700

    Add shape noise and redshift error options

--- clmm/mock_data.py
+++ clmm/mock_data.py
@@ -8,7 +8,7 @@ from astropy.table import Table
 
 class MockData(): 
     """
-    a class that generates a mock galaxy catalog around a galaxy cluster
+    a class that generates a mock background galaxy catalog around a galaxy cluster
     
     Attributes
     ----------
@@ -55,8 +55,8 @@ class MockData():
             self.config['concentration'] = 4
             
 
-    def generate(self,is_shape_noise=False, is_z_err=False):
-        '''
+    def generate(self, is_shapenoise=False, shapenoise=0.005, is_zerr=False, sigma_z=0.05):
+        """
         This generates a fake dataset of background galaxies using the Dallas group software. 
         By default, data are perfect, i.e. no shape noise, all galaxies at the same redshift.
         TODO: implement the shape noise, and redshift error options
@@ -71,18 +71,27 @@ class MockData():
         Returns:
         ---------
             astropy table containing the galaxy catalog [id, ra, dec, gamma1, gamma2, z]
-        '''
-
-        if is_shape_noise:
-            self.config['shape_noise'] = is_shape_noise
-
-        if is_z_err:
-            self.config['redshift_err'] = is_z_noise
-
-        if is_shape_noise or is_z_err:
-            raise ValueError('Options not implemented yet')
+        """
 
         ngals = self.config['ngals']
+   
+        self.z_best = np.zeros(ngals)+self.config['src_z']
+    
+        if is_zerr:
+            # introduce a redshift error on the source redshifts and generate 
+            # the corresponding Gaussian pdf
+            self.config['redshift_err'] = sigma_z
+            self.z_true = self.config['src_z'] + sigma_z*np.random.standard_normal(ngals)
+            zmin = self.config['src_z'] - 0.3
+            zmax = self.config['src_z'] + 0.3
+            self.zbins = np.arange(zmin, zmax, 0.02)
+            self.z_pdf = np.exp(-0.5*((self.zbins - self.config['src_z'])/sigma_z)**2)/np.sqrt(2*np.pi*sigma_z**2)    
+            assert np.abs(np.trapz(self.z_pdf, self.zbins) - 1) < 1e-6
+            self.pdf_grid = np.vstack(ngals*[self.z_pdf])
+        else:
+            # No redshift error
+            self.z_true = self.z_best
+
         seqnr = np.arange(ngals)
         zL = self.config['cluster_z'] # cluster redshift
         mdef = self.config['mdef']
@@ -105,7 +114,10 @@ class MockData():
         x_deg = (x_mpc/Dl)*(180./np.pi) #ra
         y_deg = (y_mpc/Dl)*(180./np.pi) #dec
 
-        gamt= testProf.deltaSigma(r_mpc)/testProf.Sc(self.config['src_z'])
+        gamt= testProf.deltaSigma(r_mpc)/testProf.Sc(self.z_true)
+        if is_shapenoise:
+            self.config['shape_noise'] = shapenoise
+            gamt = gamt + shapenoise*np.random.standard_normal(ngals)
 
         posangle = np.arctan2(y_mpc, x_mpc)
         cos2phi = np.cos(2*posangle)
@@ -114,5 +126,9 @@ class MockData():
         e1 = -gamt*cos2phi
         e2 = -gamt*sin2phi
 
-        self.catalog = Table([seqnr, -x_deg, y_deg, e1, e2, np.zeros(ngals)+self.config['src_z']], \
-                            names=('id', 'ra','dec','gamma1','gamma2', 'z'))
+        if is_zerr:
+            self.catalog = Table([seqnr, -x_deg, y_deg, e1, e2, self.z_best, self.pdf_grid], \
+                                  names=('id', 'ra','dec','gamma1','gamma2', 'z', 'z_pdf'))
+        else:
+            self.catalog = Table([seqnr, -x_deg, y_deg, e1, e2, self.z_best], \
+                                names=('id', 'ra','dec','gamma1','gamma2', 'z'))
