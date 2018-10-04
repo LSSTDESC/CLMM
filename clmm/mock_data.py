9aa69bb7b7d3338e42a9940a38b8020aa95372bb
Celine Combet <celine.combet@lpsc.in2p3.fr>
2018-10-04 02:42:54 -0700

    Docstrings

--- clmm/mock_data.py
+++ clmm/mock_data.py
@@ -7,41 +7,43 @@ from astropy.table import Table
 
 
 class MockData(): 
-    """
-    a class that generates a mock background galaxy catalog around a galaxy cluster
+    '''
+    A class that generates a mock background galaxy catalog around a galaxy cluster
     
     Attributes
     ----------
     config: dictionary
-        The configuration to generate the fake data. The cluster is located in (0,0).
-        The fields of the dictionary should be:
-        ngals: int
-            number of galaxies in the fake catalog
-        cluster_m: float
-            mass of the cluster
-        cluster_z: float
-            redshift of cluster
-        concentration: float
-            concentration of the cluster
-        src_z: float
-            redshift of background galaxies
-        cosmo: string 
-            Defines the cosmological parameter set in colossus, e.g. WMAP7-ML
-        mdef: string
-            Mass definition, e.g. '200c'
-            
+        Main properties of the mock data setup (number of galaxies, cluster mass,
+        cluster redshift, cluster concentration, source redshift, cosmo. params., 
+        mass definition)        
     catalog: astropy table
         The catalog generated given the user-defined configuration
-
-    """
+    '''
 
     
     def __init__(self, config=None):
-        """
+        '''
         Parameters
         ----------
-
-        """
+        config: dictionary
+            Main properties of the mock data setup. The cluster is located in (0,0).
+            The fields of the dictionary should be:
+            ngals: int
+                number of galaxies in the fake catalog
+            cluster_m: float
+                mass of the cluster
+            cluster_z: float
+                redshift of cluster
+            concentration: float
+                concentration of the cluster
+            src_z: float
+                redshift of background galaxies
+            cosmo: string 
+                Defines the cosmological parameter set in colossus, e.g. WMAP7-ML
+            mdef: string
+                Mass definition, e.g. '200c'     
+        '''
+        
         if config is not None:
             self.config = config
         else:
@@ -56,41 +58,41 @@ class MockData():
             
 
     def generate(self, is_shapenoise=False, shapenoise=0.005, is_zerr=False, sigma_z=0.05):
-        """
-        This generates a fake dataset of background galaxies using the Dallas group software. 
-        By default, data are perfect, i.e. no shape noise, all galaxies at the same redshift.
-        TODO: implement the shape noise, and redshift error options
+        '''
+        Generates a mock dataset of sheared background galaxies using the Dallas group software. 
 
-        Options:
+        Parameters:
         --------
-        is_shape_noise: Boolean
+        is_shapenoise: bool
             If True, noise is added to the galaxy shapes
-        is_z_err: Boolean
+        shapenoise: float
+            Amount of noise to add to the galaxy shapes
+        is_zerr: bool
             If True, a photometric redshift error is added to the background galaxy redshifts
-        
-        Returns:
-        ---------
-            astropy table containing the galaxy catalog [id, ra, dec, gamma1, gamma2, z]
-        """
+            and a gaussian pdf is created
+        sigma_z: float
+            Width of the redshift Gaussian pdf
+        '''
 
         ngals = self.config['ngals']
    
-        self.z_best = np.zeros(ngals)+self.config['src_z']
+        z_best = np.zeros(ngals)+self.config['src_z']
     
         if is_zerr:
             # introduce a redshift error on the source redshifts and generate 
             # the corresponding Gaussian pdf
             self.config['redshift_err'] = sigma_z
-            self.z_true = self.config['src_z'] + sigma_z*np.random.standard_normal(ngals)
+            z_true = self.config['src_z'] + sigma_z*np.random.standard_normal(ngals)
             zmin = self.config['src_z'] - 0.3
             zmax = self.config['src_z'] + 0.3
-            self.zbins = np.arange(zmin, zmax, 0.02)
-            self.z_pdf = np.exp(-0.5*((self.zbins - self.config['src_z'])/sigma_z)**2)/np.sqrt(2*np.pi*sigma_z**2)    
-            assert np.abs(np.trapz(self.z_pdf, self.zbins) - 1) < 1e-6
-            self.pdf_grid = np.vstack(ngals*[self.z_pdf])
+            zbins = np.arange(zmin, zmax, 0.03)
+            z_pdf = np.exp(-0.5*((zbins - self.config['src_z'])/sigma_z)**2)/np.sqrt(2*np.pi*sigma_z**2)    
+            assert np.abs(np.trapz(z_pdf, zbins) - 1) < 1e-6
+            pdf_grid = np.vstack(ngals*[z_pdf])
+            zbins_grid = np.vstack(ngals*[zbins])
         else:
             # No redshift error
-            self.z_true = self.z_best
+            z_true = z_best
 
         seqnr = np.arange(ngals)
         zL = self.config['cluster_z'] # cluster redshift
@@ -114,7 +116,7 @@ class MockData():
         x_deg = (x_mpc/Dl)*(180./np.pi) #ra
         y_deg = (y_mpc/Dl)*(180./np.pi) #dec
 
-        gamt= testProf.deltaSigma(r_mpc)/testProf.Sc(self.z_true)
+        gamt = testProf.deltaSigma(r_mpc)/testProf.Sc(z_true)
         if is_shapenoise:
             self.config['shape_noise'] = shapenoise
             gamt = gamt + shapenoise*np.random.standard_normal(ngals)
@@ -127,8 +129,8 @@ class MockData():
         e2 = -gamt*sin2phi
 
         if is_zerr:
-            self.catalog = Table([seqnr, -x_deg, y_deg, e1, e2, self.z_best, self.pdf_grid], \
-                                  names=('id', 'ra','dec','gamma1','gamma2', 'z', 'z_pdf'))
+            self.catalog = Table([seqnr, -x_deg, y_deg, e1, e2, z_best, pdf_grid, zbins_grid], \
+                                  names=('id', 'ra','dec','gamma1','gamma2', 'z', 'z_pdf', 'z_bins'))
         else:
-            self.catalog = Table([seqnr, -x_deg, y_deg, e1, e2, self.z_best], \
+            self.catalog = Table([seqnr, -x_deg, y_deg, e1, e2, z_best], \
                                 names=('id', 'ra','dec','gamma1','gamma2', 'z'))
