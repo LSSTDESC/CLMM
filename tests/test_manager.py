8df94f91842ddcba173a7fca32e6b527226a1934
Camille Avestruz <camille.avestruz@yale.edu>
2018-10-22 06:23:56 -0500

    added stubs to test_manager

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -1,12 +1,21 @@
 ''' 
-
+Tests for manager
 '''
+
+
 import os, sys
 DIR = os.path.dirname(os.path.realpath(__file__))
 sys.path.append('/'.join(DIR.split('/')[:-1]))
 
 from clmm.core import manager
+from test_galaxycluster import *
 
-test_creator = 'Mitch'
-test_creator_diff = 'Witch'
+def test_apply() :
+    pass
+
+def test_prepare() :
+    pass
+
+def test_deliver() :
+    pass
 
