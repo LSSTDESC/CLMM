e18b9c11ad9af426906f19957c63462d1dbba321
Camille Avestruz <camille.avestruz@yale.edu>
2018-10-22 06:16:58 -0500

    added start to tests for manager.py

---
+++ tests/test_manager.py
@@ -0,0 +1,12 @@
+''' 
+
+'''
+import os, sys
+DIR = os.path.dirname(os.path.realpath(__file__))
+sys.path.append('/'.join(DIR.split('/')[:-1]))
+
+from clmm.core import manager
+
+test_creator = 'Mitch'
+test_creator_diff = 'Witch'
+
