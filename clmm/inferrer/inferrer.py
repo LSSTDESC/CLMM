6bcff3d276a1b9cdc544843814e327bad34dfa1d
Camille Avestruz <briarrose5187@gmail.com>
2018-10-23 05:39:25 -0700

    description of inferrer object added

--- clmm/inferrer/inferrer.py
+++ clmm/inferrer/inferrer.py
@@ -1,10 +1,6 @@
 '''
-
 Inferrer object which has methods to both run chains per cluster
 object (or on a stacked cluster object), read in existing chains per
 cluster object, and conduct importance sampling on a collection of
 chains.
-
-
 '''
-
