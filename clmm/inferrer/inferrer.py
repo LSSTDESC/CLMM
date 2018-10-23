98170ac4cf118f37078e333f58236fec33a7c034
Camille Avestruz <briarrose5187@gmail.com>
2018-10-23 04:35:45 -0700

    started inferrer module

---
+++ clmm/inferrer/inferrer.py
@@ -0,0 +1,10 @@
+'''
+
+Inferrer object which has methods to both run chains per cluster
+object (or on a stacked cluster object), read in existing chains per
+cluster object, and conduct importance sampling on a collection of
+chains.
+
+
+'''
+
