136b2f9b233000d73ed5e5aa73d8759d59e723a4
matthewkirby <matthewkirby@email.arizona.edu>
2019-07-21 22:00:16 +0200

    Tell docs, setup, and travis to ignore the crypt directories

--- clmm/crypt/__init__.py
+++
@@ -1,7 +0,0 @@
-"""A module to fit galaxy cluster masses"""
-from __future__ import absolute_import
-
-from .models import *
-from .summarizer import *
-from .core import *
-from .mock_data import *
