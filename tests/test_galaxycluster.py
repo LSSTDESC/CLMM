144640bdca949eb9644346c24bf5663001a5b165
matthewkirby <matthewkirby@email.arizona.edu>
2019-07-22 16:02:56 +0200

    Changed unique_id functionality

--- tests/test_galaxycluster.py
+++ tests/test_galaxycluster.py
@@ -7,7 +7,7 @@ from astropy.table import Table
 
 
 def test_initialization():
-    testdict1 = {'unique_id': 1, 'ra': 161.3, 'dec': 34., 'z': 0.3, 'richness': 103., 'galcat': Table()}
+    testdict1 = {'unique_id': '1', 'ra': 161.3, 'dec': 34., 'z': 0.3, 'richness': 103., 'galcat': Table()}
     cl1 = clmm.GalaxyCluster(**testdict1)
 
     testing.assert_equal(testdict1['unique_id'], cl1.id)
@@ -42,12 +42,13 @@ def test_integrity(): # Converge on name
     testing.assert_raises(TypeError, clmm.GalaxyCluster, unique_id=1, ra=161.3, dec=34., z='0.3', richness=103., galcat=Table())
     testing.assert_raises(TypeError, clmm.GalaxyCluster, unique_id=1, ra=161.3, dec=34., z=0.3, richness='103.', galcat=Table())
 
-    # Test that id can support any type
-    clmm.GalaxyCluster(unique_id=1, ra=161.3, dec=34., z=0.3, richness=103., galcat=Table())
-    clmm.GalaxyCluster(unique_id='1', ra=161.3, dec=34., z=0.3, richness=103., galcat=Table())
-    clmm.GalaxyCluster(unique_id=1.0, ra=161.3, dec=34., z=0.3, richness=103., galcat=Table())
+    # Test that id can support numbers and strings
+    assert isinstance(clmm.GalaxyCluster(unique_id=1, ra=161.3, dec=34., z=0.3, richness=103., galcat=Table()).unique_id, str)
+    assert clmm.GalaxyCluster(unique_id=1.0, ra=161.3, dec=34., z=0.3, richness=103., galcat=Table()).unique_id == '1'
+    assert isinstance(clmm.GalaxyCluster(unique_id='1', ra=161.3, dec=34., z=0.3, richness=103., galcat=Table()).unique_id, str)
 
-
+# def test_save_load():
+#     clmm.GalaxyCluster(unique_id=1.0, ra=161.3, dec=34., z=0.3, richness=103., galcat=Table())
 
 
 # def test_find_data():
