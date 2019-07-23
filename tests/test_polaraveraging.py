786c23f3269455446dcd9d4a9741d5eac9c6cebc
Lucie Baumont <lucie.baumont@stonybrook.edu>
2019-07-23 18:01:00 +0200

    even more tests for make_bins

--- tests/test_polaraveraging.py
+++ tests/test_polaraveraging.py
@@ -11,6 +11,10 @@ from polaraveraging import *
 def test_make_bins():
     ## do something
 
+    testing.assert_equal(len( polaraveraging._make_bins(1,10,9,False)),10 )
+    testing.assert_allclose( polaraveraging._make_bins(1,10,9,False) , np.arange(1.,11.) )
+    testing.assert_allclose( polaraveraging._make_bins(1,10000,4,True) ,10.**(np.arange(5)) )
+    
     testing.assert_raises(TypeError, polaraveraging._make_bins, rmin='glue', rmax=10, n_bins=9, log_bins=False)
     testing.assert_raises(TypeError, polaraveraging._make_bins, rmin=1, rmax='glue', n_bins=9, log_bins=False)
     testing.assert_raises(TypeError, polaraveraging._make_bins, rmin=1, rmax=10, n_bins='glue', log_bins=False)
