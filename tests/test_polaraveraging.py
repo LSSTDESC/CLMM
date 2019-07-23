1a9d4e4ea5e159a737d8bbb3d31d20a85c2b9e14
Martin Sommer <mnord@astro.uni-bonn.de>
2019-07-23 15:19:50 +0200

    basic structure for test_polaraveraging

---
+++ tests/test_polaraveraging.py
@@ -0,0 +1,14 @@
+"""
+Tests for polaraveraging.py
+"""
+from numpy import testing
+import clmm
+from astropy.table import Table
+import os
+
+def test_theta_g():
+    ## do something
+
+
+if __name__ == "__main__":
+    test_theta_g()
