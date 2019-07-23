4c5f83e2ff8049fcad21b29d49a3980c99b4b58b
Lucie Baumont <lucie.baumont@stonybrook.edu>
2019-07-23 17:13:36 +0200

    test_polaraveraging

--- tests/test_polaraveraging.py
+++ tests/test_polaraveraging.py
@@ -2,13 +2,28 @@
 Tests for polaraveraging.py
 """
 from numpy import testing
-import clmm
+#import clmm
 from astropy.table import Table
 import os
+import polaraveraging
+from polaraveraging import *
 
-def test_theta_g():
+def test_make_bins():
     ## do something
+
+    testing.assert_raises(TypeError, polaraveraging._make_bins, rmin='glue', rmax=10, n_bins=9, log_bins=False)
+    testing.assert_raises(TypeError, polaraveraging._make_bins, rmin=1, rmax='glue', n_bins=9, log_bins=False)
+    testing.assert_raises(TypeError, polaraveraging._make_bins, rmin=1, rmax=10, n_bins='glue', log_bins=False)
+#    testing.assert_raises(TypeError, polaraveraging._make_bins, rmin=1, rmax=10, n_bins=9, log_bins='glue')
+
+    testing.assert_raises(ValueError, polaraveraging._make_bins, rmin=1, rmax=10, n_bins=-4, log_bins=False)
+    testing.assert_raises(ValueError, polaraveraging._make_bins, rmin=1, rmax=-10, n_bins=9, log_bins=False)
+  #  testing.assert_raises(ValueError, polaraveraging._make_bins, rmin=1, rmax=10, n_bins=0, log_bins=False)
+   # testing.assert_raises(ValueError, polaraveraging._make_bins, rmin=1, rmax=10, n_bins=9, log_bins=4)
+
+    
+    
 
 
 if __name__ == "__main__":
-    test_theta_g()
+    test_make_bins()
