022751709cacc73adc770958100e1745e6e320c5
Lucie Baumont <lucie.baumont@stonybrook.edu>
2019-07-24 11:26:25 +0200

    tests

--- tests/test_polaraveraging.py
+++ tests/test_polaraveraging.py
@@ -10,26 +10,23 @@ from polaraveraging import *
 
 def test_make_bins():
     ## do something
-
-    testing.assert_equal(len( polaraveraging._make_bins(1,10,9,False)),10 )
-    testing.assert_allclose( polaraveraging._make_bins(1,10,9,False) , np.arange(1.,11.) )
-    testing.assert_allclose( polaraveraging._make_bins(1,10000,4,True) ,10.**(np.arange(5)) )
+     pass
+    testing.assert_equal(len( polaraveraging.make_bins(1,10,9,False)),10 )
+    testing.assert_allclose( polaraveraging.make_bins(1,10,9,False) , np.arange(1.,11.) )
+    testing.assert_allclose( polaraveraging.make_bins(1,10000,4,True) ,10.**(np.arange(5)) )
     
-    testing.assert_raises(TypeError, polaraveraging._make_bins, rmin='glue', rmax=10, n_bins=9, log_bins=False)
-    testing.assert_raises(TypeError, polaraveraging._make_bins, rmin=1, rmax='glue', n_bins=9, log_bins=False)
-    testing.assert_raises(TypeError, polaraveraging._make_bins, rmin=1, rmax=10, n_bins='glue', log_bins=False)
-    testing.assert_raises(TypeError, polaraveraging._make_bins, rmin=1, rmax=10, n_bins=9, log_bins='glue')
+    testing.assert_raises(TypeError, polaraveraging.make_bins, rmin='glue', rmax=10, n_bins=9, log_bins=False)
+    testing.assert_raises(TypeError, polaraveraging.make_bins, rmin=1, rmax='glue', n_bins=9, log_bins=False)
+    testing.assert_raises(TypeError, polaraveraging.make_bins, rmin=1, rmax=10, n_bins='glue', log_bins=False)
+    testing.assert_raises(TypeError, polaraveraging.make_bins, rmin=1, rmax=10, n_bins=9, log_bins='glue')
 
-    testing.assert_raises(ValueError, polaraveraging._make_bins, rmin=1, rmax=10, n_bins=-4, log_bins=False)
-    testing.assert_raises(ValueError, polaraveraging._make_bins, rmin=1, rmax=-10, n_bins=9, log_bins=False)
-    testing.assert_raises(ValueError, polaraveraging._make_bins, rmin=1, rmax=10, n_bins=0, log_bins=False)
-    testing.assert_raises(TypeError, polaraveraging._make_bins, rmin=1, rmax=10, n_bins=9.9, log_bins=False)
-
-    
-    
-
+    testing.assert_raises(ValueError, polaraveraging.make_bins, rmin=1, rmax=10, n_bins=-4, log_bins=False)
+    testing.assert_raises(ValueError, polaraveraging.make_bins, rmin=1, rmax=-10, n_bins=9, log_bins=False)
+    testing.assert_raises(ValueError, polaraveraging.make_bins, rmin=1, rmax=10, n_bins=0, log_bins=False)
+    testing.assert_raises(TypeError, polaraveraging.make_bins, rmin=1, rmax=10, n_bins=9.9, log_bins=False)
 
 def test_compute_g_x():
+    pass
     data = np.array([[0.01, 0.02, 0.01], # g1
                      [0.01, 0.02, 0.03], # g2
                      [1.0, 2.0, 0.5]]) # phi
@@ -52,7 +49,23 @@ def test_compute_g_x():
     # phi=0, phi=90
     # g1, g2 = 0
     # phi <0, >180, also =
-    
+
+def test_compute_radial_averages():
+
+    #testing input types
+    testing.assert_raises(TypeError, clmm._compute_radial_averages, radius="glue", g=10, bins=[np.arange(1.,16.)])
+    testing.assert_raises(TypeError, clmm._compute_radial_averages, radius=np.arange(1.,10.), g="glue", bins=[np.arange(1.,16.)])  
+    testing.assert_raises(TypeError, clmm._compute_radial_averages, radius=np.arange(1.,10.), g=np.arange(1.,10.), bins='glue') 
+
+
+    #want radius and g to have same number of entries
+    testing.assert_raises(TypeError, clmm._compute_radial_averages, radius=np.arange(1.,10.), g=np.arange(1.,7.), bins=[np.arange(1.,16.)])
+    #want nbins <=2 
+    testing.assert_raises(TypeError, clmm._compute_radial_averages, radius=np.arange(1.,10.), g=np.arange(1.,10.), bins=1)
+    #want binning to encompass entire radial range
+    testing.assert_raises(ValueError, clmm._compute_radial_averages, radius=np.arange(1.,10.), g=np.arange(1.,10.), bins=[1,6,7])
+    testing.assert_raises(ValueError, clmm._compute_radial_averages, radius=np.arange(1.,10.), g=np.arange(1.,10.), bins=[5,6,7]) 
+      
 
 if __name__ == "__main__":
-    test_make_bins()
+    test_compute_radial_averages()
