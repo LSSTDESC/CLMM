563559ae6dcef5a7aa51d11507d38c1da96c30f3
Lucie Baumont <lucie.baumont@stonybrook.edu>
2019-07-24 12:41:30 +0200

    minor bug fixes

--- clmm/polaraveraging.py
+++ clmm/polaraveraging.py
@@ -175,7 +175,7 @@ def _compute_theta_phi(ra_l, dec_l, ra_s, dec_s, sky="flat"):
         ## subtract 360 deg from RA angles > 180 deg
         dx[dx>np.pi] = dx[dx>np.pi] - 2.*np.pi
         ## add 360 deg to RA angles < -180 deg
-        dx[dx<-np.pi] = dx[dx<-np.pi] + 2.np.pi 
+        dx[dx<-np.pi] = dx[dx<-np.pi] + 2.*np.pi 
         theta =  np.sqrt(dx**2 + dy**2)
     elif sky == "curved":
         raise ValueError("Curved sky functionality not yet supported!")
@@ -369,6 +369,13 @@ def _compute_radial_averages(radius, g, bins=None):
     gerr_profile: array_like, float
         Standard deviation of shear per bin
     """
+
+    if type(radius) != np.ndarray:
+        raise TypeError("radius must be an array")
+    if type(g) != np.ndarray:
+        raise TypeError("g must be an array")
+        
+    
     if np.any(bins) == None:
         nbins = 10
         bins = np.linspace(np.min(radius), np.max(radius), nbins)
