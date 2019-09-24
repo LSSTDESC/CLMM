2055e7501ec85834e589db2ae43777439f69e874
Marina Ricci <mricci@cca010.in2p3.fr>
2019-09-24 18:36:04 +0200

    modifications mainly in the binning functions

--- clmm/polaraveraging.py
+++ clmm/polaraveraging.py
@@ -11,7 +11,9 @@ from scipy.stats import binned_statistic
 from astropy.coordinates import SkyCoord
 from astropy.cosmology import FlatLambdaCDM
 from astropy.table import Table
-
+#123123 
+#import clmm.utils
+#123123 
 
 # def _astropy_to_CCL_cosmo_object(astropy_cosmology_object): # 7481794
 #     """Generates a ccl cosmology object from an GCR or astropy cosmology object.
@@ -31,8 +33,8 @@ def compute_shear(cluster=None, ra_lens=None, dec_lens=None, ra_source_list=None
                   add_to_cluster=True):
     r"""Computes tangential shear, cross shear, and angular separation
 
-    To compute the shear, we need the right ascension and declination of the len and of
-    all of the sources. We also need the two shears or ellipticities of all of the sources.
+    To compute the shear, we need the right ascension and declination of the lens and of
+    all of the sources. We also need the two shear components of all of the sources.
 
     These quantities can be handed to `compute_shear` in three ways
 
@@ -49,7 +51,7 @@ def compute_shear(cluster=None, ra_lens=None, dec_lens=None, ra_source_list=None
         cluster.compute_shear()
 
     The angular separation between the source and the lens, :math:`\theta`, and the azimuthal
-    position of the source relative to the lens, :math:`\phi`, are computed within and the
+    position of the source relative to the lens, :math:`\phi`, are computed within the function and the
     angular separation is returned.
 
     In the flat sky approximation, these angles are calculated using
@@ -61,8 +63,8 @@ def compute_shear(cluster=None, ra_lens=None, dec_lens=None, ra_source_list=None
         \tan\phi = & \frac{\delta_s - \delta_l}{\left(\alpha_l - \alpha_s\right)\cos(\delta_l)}
 
     The tangential, :math:`g_t`, and cross, :math:`g_x`, shears are calculated using the two
-    measured ellipticities or shears of the source galaxies following
-    Schrabback et al. (2018), arXiv:1611:03866
+   shear components :math:`g_1` and :math:`g_2` of the source galaxies, following Eq.7 and Eq.8
+   in Schrabback et al. (2018), arXiv:1611:03866
 
     .. math::
 
@@ -74,7 +76,7 @@ def compute_shear(cluster=None, ra_lens=None, dec_lens=None, ra_source_list=None
     ----------
     cluster: GalaxyCluster, optional
         Instance of `GalaxyCluster()` and must contain right ascension and declinations of both
-        the lens and sources and the two shears or ellipticities of all of the sources. If this
+        the lens and sources and the two shear components all of the sources. If this
         object is specified, right ascension, declination, and shear inputs are ignored.
     ra_lens: float, optional
         Right ascension of the lensing cluster
@@ -85,9 +87,9 @@ def compute_shear(cluster=None, ra_lens=None, dec_lens=None, ra_source_list=None
     dec_source_list: array_like, optional
         Declinations of each source galaxy
     shear1: array_like, optional
-        The measured shears or ellipticities of the source galaxies
+        The measured shear of the source galaxies
     shear2: array_like, optional
-        The measured shears or ellipticities of the source galaxies
+        The measured shear of the source galaxies
     geometry: str, optional
         Sky geometry to compute angular separation.
         Flat is currently the only supported option.
@@ -184,32 +186,32 @@ def _compute_lensing_angles_flatsky(ra_lens, dec_lens, ra_source_list, dec_sourc
 
 
 def _compute_tangential_shear(shear1, shear2, phi):
-    r"""Compute the tangential shear given the two shears and azimuthal position for
+    r"""Compute the tangential shear given the two shears and azimuthal positions for
     a single source or list of sources.
 
-    We compute the cross shear following Schrabback et al. 2018, arXiv:1611:03866
+    We compute the cross shear following Eq. 7 of Schrabback et al. 2018, arXiv:1611:03866
     .. math::
         g_t = -\left( g_1\cos\left(2\phi\right) + g_2\sin\left(2\phi\right)\right)
 
     For extended descriptions of parameters, see `compute_shear()` documentation.
     """
-    return -(shear1*np.cos(2.*phi) + shear2*np.sin(2.*phi))
+    return - (shear1 * np.cos(2.*phi) + shear2 * np.sin(2.*phi))
 
 
 def _compute_cross_shear(shear1, shear2, phi):
     r"""Compute the cross shear given the two shears and azimuthal position for a single
     source of list of sources.
 
-    We compute the cross shear following Schrabback et al. 2018, arXiv:1611:03866
+    We compute the cross shear following Eq. 8 of Schrabback et al. 2018, arXiv:1611:03866
     .. math::
         g_x = -g_1 \sin\left(2\phi\right) + g_2\cos\left(2\phi\right)
 
     For extended descriptions of parameters, see `compute_shear()` documentation.
     """
-    return -shear1*np.sin(2.*phi) + shear2*np.cos(2.*phi)
+    return - shear1 * np.sin(2.*phi) + shear2 * np.cos(2.*phi)
 
 
-def make_shear_profile(cluster, angsep_units, bin_units, bins=None, cosmo=None,
+def make_shear_profile(cluster, angsep_units, bin_units, bins=10, cosmo=None,
                        add_to_cluster=True):
     r"""Compute the shear profile of the cluster
 
@@ -238,10 +240,10 @@ def make_shear_profile(cluster, angsep_units, bin_units, bins=None, cosmo=None,
         Units to use for the radial bins of the shear profile
         Allowed Options = ["radians", deg", "arcmin", "arcsec", kpc", "Mpc"]
     bins : array_like, optional
-        User defined bins to use for the shear profile. If a scalar is provided, create that many
-        bins between the minimum and maximum angular separations in bin_units. If an list is
-        provided, use that as the bin extents. If nothing is provided, default to 10 equally
-        spaced bins.
+        User defined bins to use for the shear profile. If a list is provided, use that as 
+        the bin edges. If a scalar is provided, create that many equally spaced bins between
+        the minimum and maximum angular separations in bin_units. If nothing is provided, 
+        default to 10 equally spaced bins.
     cosmo: dict, optional
         Cosmology parameters to convert angular separations to physical distances
     add_to_cluster: bool, optional
@@ -251,29 +253,39 @@ def make_shear_profile(cluster, angsep_units, bin_units, bins=None, cosmo=None,
     -------
     profile : astropy.table.Table
         Output table containing the radius grid points, the tangential and cross shear profiles
-        on that grid, and the errors in the two shear profiles.
+        on that grid, and the errors in the two shear profiles. The errors are defined as the standard errors
+        in each bin.
     """
     if not all([t_ in cluster.galcat.columns for t_ in ('gt', 'gx', 'theta')]):
         raise TypeError('Shear information is missing in galaxy catalog must have tangential\
                         and cross shears (gt,gx). Run compute_shear first!')
 
+#123123 
     # Check to see if we need to do a unit conversion
     if angsep_units is not bin_units:
         source_seps = _theta_units_conversion(cluster.galcat['theta'], angsep_units, bin_units,
                                               z_cl=cluster.z, cosmo=cosmo)
     else:
         source_seps = cluster.galcat['theta']
+#123123 
 
+#123123    
     # Make bins if they are not provided
     if not hasattr(bins, '__len__'):
-        bins = np.linspace(np.min(source_seps), np.max(source_seps), bins)
+        bins = make_bins(np.min(source_seps), np.max(source_seps), bins)
+#123123
 
-    # Compute the binned average shears
-    r_avg, gt_avg, gt_std = _compute_radial_averages(source_seps, cluster.galcat['gt'].data,
+#123123
+    # Compute the binned average shears and associated errors
+    r_avg, gt_avg, gt_std, gt_counts = _compute_radial_averages(source_seps, cluster.galcat['gt'].data,
                                                      bins=bins)
-    r_avg, gx_avg, gx_std = _compute_radial_averages(source_seps, cluster.galcat['gx'].data,
-                                                     bins=bins)
-    profile_table = Table([bins[:-1], r_avg, bins[1:], gt_avg, gt_std, gx_avg, gx_avg],
+    r_avg, gx_avg, gx_std, gx_counts = _compute_radial_averages(source_seps, cluster.galcat['gx'].data,
+                                                    bins=bins)
+#123123
+    gt_err = gt_std / gt_counts
+    gx_err = gx_std / gx_counts
+    
+    profile_table = Table([bins[:-1], r_avg, bins[1:], gt_avg, gt_err, gx_avg, gx_err],
                           names=('radius_min', 'radius', 'radius_max', 'gt', 'gt_err',
                                  'gx', 'gx_err'))
 
@@ -282,60 +294,6 @@ def make_shear_profile(cluster, angsep_units, bin_units, bins=None, cosmo=None,
         cluster.profile_bin_units = bin_units
 
     return profile_table
-
-
-def _theta_units_conversion(source_seps, input_units, output_units, z_cl=None, cosmo=None):
-    """Convert source separations from input_units to output_units
-
-    Parameters
-    ----------
-    source_seps : array_like
-        Separation between the lens and each source galaxy on the sky
-    input_units : str
-        Units of the input source_seps
-    output_units : str
-        Units to convert source_seps to
-        Options = ["rad", deg", "arcmin", "arcsec", kpc", "Mpc"]
-    z_cl :  float, optional
-	Cluster redshift. Required to convert to physical distances.
-    cosmo : astropy.cosmology.core.FlatLambdaCDM, optional
-        Cosmology object. Required to convert to physical distances.
-
-    Returns
-    -------
-    new_radii : array_like 
-        Source-lens separation in output_units.
-    """
-    from astropy import units as u
-    units_bank = {"radians": u.rad, "deg": u.deg, "arcmin": u.arcmin, "arcsec": u.arcsec,
-                  "kpc": u.kpc, "Mpc": u.Mpc}
-
-    # Check to make sure both the input_units and output_units are supported
-    if not input_units in units_bank:
-        raise ValueError("Input units{} for separation not supported".format(input_units))
-    if not output_units in units_bank:
-        raise ValueError("Output units{} for separation not supported".format(output_units))
-
-    # Set input_units on source_seps
-    source_seps = source_seps*units_bank[input_units]
-
-    # Convert to output units and return
-    if 'pc' in output_units:
-        if z_cl is None or cosmo is None:
-            raise ValueError("Cluster redshift and cosmology object required to convert to\
-                              physical units")
-        out_units_obj = units_bank[output_units]
-        angular_diameter_distance = cosmo.angular_diameter_distance(z_cl).to(out_units_obj).value
-        # if isinstance(cosmo, astropy.cosmology.core.FlatLambdaCDM): # astropy cosmology type
-        #     Da = cosmo.angular_diameter_distance(z_cl).to(unit_).value
-        # elif isinstance(cosmo, ccl.core.Cosmology): # astropy cosmology type # 7481794
-        #     Da = ccl.comoving_angular_distance(cosmo, 1/(1+z_cl)) / (1+z_cl) * u.Mpc.to(unit_)
-        # else:
-        #     raise ValueError("cosmo object (%s) not an astropy or ccl cosmology"%str(cosmo))
-        return source_seps.value*angular_diameter_distance
-
-    else:
-        return source_seps.to(units_bank[output_units]).value
 
 
 def _compute_radial_averages(distances, measurements, bins):
@@ -388,57 +346,111 @@ def _compute_radial_averages(distances, measurements, bins):
     #         r_profile[i] = np.average(distances[index])
     #         y_profile[i] = np.average(measurements[index])
     #         yerr_profile[i] = np.std(measurements[index])/np.sqrt(float(len(index)))
-
+    
     r_profile, _, _ = binned_statistic(distances, distances, statistic='mean', bins=bins)
     y_profile, _, _ = binned_statistic(distances, measurements, statistic='mean', bins=bins)
     yerr_profile, _, _ = binned_statistic(distances, measurements, statistic='std', bins=bins)
+    counts_profile, _, _ = binned_statistic(distances, measurements, statistic='count', bins=bins)
 
-    return r_profile, y_profile, yerr_profile
-
-
-
+    return r_profile, y_profile, yerr_profile, counts_profile
 
 
+#123123 
+def make_bins(rmin, rmax, n_bins=10, log10_bins=False, method='equal'):
+    """Define the bins edges corresponding to the binning of data using user-defined method and parameters.
+     The binning method currently supported returns equaly sized bins. 
 
+     Parameters
+     ----------
+     rmin, rmax,: float
+         minimum and maximum bin edges wanted (any units).
+     n_bins: float
+         number of bins you want to create, default to 10.
+     log10_bins: bool
+         set to 'True' for binning in log space of base 10, default to False, 
+     method : str
+         binning method used, 'equal' is currently the only supported option.
 
+     Returns
+     -------
+     binedges: array_like, float
+         n_bins+1 dimensional array that defines bin edges
+    """
+    if (rmax < rmin):
+        raise ValueError("rmax should be larger than rmin")
+    if n_bins <= 0:
+        raise ValueError("n_bins must be > 0")
+    if type(log10_bins) != bool:
+        raise TypeError("log_bins must be type bool")
+    if type(n_bins) != int:
+        raise TypeError("You need an integer number of bins")
 
-# def make_bins(rmin, rmax, n_bins=10, log_bins=False):
-#     """Define equal sized bins with an array of n_bins+1 bin edges
-#
-#     Parameters
-#     ----------
-#     rmin, rmax,: float
-#         minimum and and maximum range of data (any units)
-#     n_bins: float
-#         number of bins you want to create
-#     log_bins: bool
-#         set to 'True' equal sized bins in log space
-#
-#     Returns
-#     -------
-#     binedges: array_like, float
-#         n_bins+1 dimensional array that defines bin edges
-#     """
-#     if rmax < rmin:
-#         raise ValueError("rmax should be larger than rmin")
-#     if n_bins <= 0:
-#         raise ValueError("n_bins must be > 0")
-#     if type(log_bins) != bool:
-#         raise TypeError("log_bins must be type bool")
-#     if type(n_bins) != int:
-#         raise TypeError("You need an integer number of bins")
-#
-#     if log_bins == True:
-#         rmin = np.log(rmin)
-#         rmax = np.log(rmax)
-#         logbinedges = np.linspace(rmin, rmax, n_bins+1, endpoint=True)
-#         binedges = np.exp(logbinedges)
-#     else:
-#         binedges = np.linspace(rmin, rmax, n_bins+1, endpoint=True)
-#
-#     return binedges
+    if method == 'equal':
+        if log10_bins == True:
+            binedges = np.logspace(np.log10(rmin), np.log10(rmax), n_bins+1, endpoint=True)
+        else:
+            binedges = np.linspace(rmin, rmax, n_bins+1, endpoint=True)
+    else:
+        raise NotImplementedError("Binning method '{}' is not currently supported".format(method))
+        
+    return binedges
+#123123 
 
+#123123 
+def _theta_units_conversion(source_seps, input_units, output_units, z_cl=None, cosmo=None):
+    """Convert source separations from input_units to output_units
 
+    Parameters
+    ----------
+    source_seps : array_like
+        Separation between the lens and each source galaxy on the sky
+    input_units : str
+        Units of the input source_seps
+    output_units : str
+        Units to convert source_seps to
+        Options = ["rad", deg", "arcmin", "arcsec", kpc", "Mpc"]
+    z_cl :  float, optional
+	Cluster redshift. Required to convert to physical distances.
+    cosmo : astropy.cosmology.core.FlatLambdaCDM, optional
+        Cosmology object. Required to convert to physical distances.
+
+    Returns
+    -------
+    new_radii : array_like 
+        Source-lens separation in output_units.
+    """
+    from astropy import units as u
+    units_bank = {"radians": u.rad, "deg": u.deg, "arcmin": u.arcmin, "arcsec": u.arcsec,
+                  "kpc": u.kpc, "Mpc": u.Mpc}
+
+    # Check to make sure both the input_units and output_units are supported
+    if not input_units in units_bank:
+        raise ValueError("Input units{} for separation not supported".format(input_units))
+    if not output_units in units_bank:
+        raise ValueError("Output units{} for separation not supported".format(output_units))
+
+    # Set input_units on source_seps
+    source_seps = source_seps*units_bank[input_units]
+
+    # Convert to output units and return
+    if 'pc' in output_units:
+        if z_cl is None or cosmo is None:
+            raise ValueError("Cluster redshift and cosmology object required to convert to\
+                              physical units")
+        out_units_obj = units_bank[output_units]
+        angular_diameter_distance = cosmo.angular_diameter_distance(z_cl).to(out_units_obj).value
+        # if isinstance(cosmo, astropy.cosmology.core.FlatLambdaCDM): # astropy cosmology type
+        #     Da = cosmo.angular_diameter_distance(z_cl).to(unit_).value
+        # elif isinstance(cosmo, ccl.core.Cosmology): # astropy cosmology type # 7481794
+        #     Da = ccl.comoving_angular_distance(cosmo, 1/(1+z_cl)) / (1+z_cl) * u.Mpc.to(unit_)
+        # else:
+        #     raise ValueError("cosmo object (%s) not an astropy or ccl cosmology"%str(cosmo))
+        return source_seps.value*angular_diameter_distance
+
+    else:
+        return source_seps.to(units_bank[output_units]).value
+#123123 
+    
 def plot_profiles(cluster, r_units=None):
     """Plot shear profiles for validation
 
