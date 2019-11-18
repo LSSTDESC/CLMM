c9baf5eec3cadc9f718f0947541e72667b1f3dfb
Alex Malz <aimalz@nyu.edu>
2019-11-18 23:29:43 +0100

    Revert "issue/113/demo_dc2 (#113)" (#172)
    This reverts commit cfa9d193abe70fb11fcb10140bec48c2bacbd04e.

--- clmm/galaxycluster.py
+++ clmm/galaxycluster.py
@@ -2,10 +2,7 @@
 The GalaxyCluster class
 """
 import pickle
-import astropy.table
 from astropy.table import Table
-# from typing import Union
-
 
 def load_cluster(filename, **kwargs):
     """Loads GalaxyCluster object from filename using Pickle"""
