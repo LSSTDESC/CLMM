677bb9740880e10579f077dd34b96ce2a53fe78a
Matthew Ho <mho1@cori07.nersc.gov>
2019-11-18 17:06:30 -0800

    added documentation and clarity to io.py. Appropriately updated the load_from_DC2.ipynb example notebook

--- clmm/lsst/io.py
+++ clmm/lsst/io.py
@@ -7,7 +7,32 @@ import numpy as np
 from astropy.table import Table
 from clmm import GalaxyCluster
 
-def load_from_dc2(N, catalog, save_dir, verbose=False):
+def load_from_dc2_rect(N, catalog, save_dir, ra_range=(-0.3, 0.3), dec_range=(-0.3, 0.3),
+                       z_range=(0.1, 1.5), verbose=False):
+    """Load random clusters from DC2 using GCRCatalogs
+    
+    This function saves a set of random clusters loaded from a DC2 catalog. The galaxies
+    selected for each cluster are cut within a rectangular projected region around the true 
+    cluster center. The parameters for this cut are specified by the ra_range, dec_range,
+    and z_range arguments.
+    
+    Parameters
+    ----------
+    N: int
+        Number of clusters to load and save. 
+    catalog: str
+        Name of catalog (without '.yaml')
+    save_dir: str
+        Path to directory in which to save cluster objects
+    ra_range: length-2 tuple of floats, optional
+        Range of right ascension values (in degrees) to cut galaxies around the cluster center
+    dec_range: length-2 tuple of floats, optional
+        Range of declination values (in degrees)to cut galaxies around the cluster center
+    z_range: length-2 tuple of floats, optional
+        Range of redshift values (in degrees)to cut galaxies around the cluster center
+    verbose: bool
+        Sets the function to print the id of each cluster while loading
+    """
     import GCRCatalogs
 
     catalog = GCRCatalogs.load_catalog(catalog)
@@ -16,30 +41,24 @@ def load_from_dc2(N, catalog, save_dir, verbose=False):
                                    filters=['halo_mass > 1e14','is_central==True'])
 
     for i in np.random.choice(range(len(halos)), N, replace=False):
-
+        # specify cluster information
         cl_id = halos['galaxy_id'][i]
         cl_ra = halos['ra'][i]
         cl_dec = halos['dec'][i]
         cl_z = halos['redshift'][i]
-        cl_m = halos['halo_mass'][i]
         
         if verbose:
             print('Loading cluster %s'%cl_id)
 
         # get galaxies around cluster
-        ra_min, ra_max = cl_ra-0.3, cl_ra+0.3
-        dec_min, dec_max = cl_dec-0.3, cl_dec+0.3
-        z_min = cl_z + 0.1
-        z_max = 1.5
-
         coord_filters = [
-            'ra >= %s'%ra_min,
-            'ra < %s'%ra_max,
-            'dec >= %s'%dec_min,
-            'dec < %s'%dec_max,
+            'ra >= %s'%(cl_ra+ra_range[0]),
+            'ra < %s'%(cl_ra+ra_range[1]),
+            'dec >= %s'%(cl_dec+dec_range[0]),
+            'dec < %s'%(cl_dec+dec_range[1]),
         ]
-        z_filters = ['redshift >= %s'%z_min,
-                     'redshift < %s'%z_max]
+        z_filters = ['redshift >= %s'%(cl_z+z_range[0]),
+                     'redshift < %s'%(cl_z+z_range[1])]
 
         gals = catalog.get_quantities(['galaxy_id', 'ra', 'dec', 
                                        'shear_1', 'shear_2',
@@ -56,9 +75,9 @@ def load_from_dc2(N, catalog, save_dir, verbose=False):
                    gals['redshift'], gals['convergence']],
                   names=('galaxy_id', 'ra','dec', 'e1', 'e2', 'z', 'kappa'))
 
-
+        # save as GalaxyCluster object
         c = GalaxyCluster(unique_id=int(cl_id), ra=cl_ra, dec=cl_dec, z=cl_z,
                           galcat=t)
 
-        c.save(os.path.join(save_dir, '%s.p'%cl_id))
+        c.save(os.path.join(save_dir, '%i.p'%cl_id))
 
