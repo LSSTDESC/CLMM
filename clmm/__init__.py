81201e252410085de7fea2ad38d71a157a1bf45b
matthewkirby <matthewkirby@email.arizona.edu>
2019-11-19 16:07:27 -0600

    Made additions to the clmm namespace explicit and omit private members. These members can still be accessed by clmm.polaraveraging._private_function

--- clmm/__init__.py
+++ clmm/__init__.py
@@ -1,6 +1,4 @@
-from .galaxycluster import *
-from .gcdata import *
-from .polaraveraging import *
-from .modeling import *
-
-from . import lsst
+from .galaxycluster import load_cluster, GalaxyCluster
+from .polaraveraging import compute_shear, make_shear_profile
+from .modeling import cclify_astropy_cosmo, get_3d_density, predict_surface_density, predict_excess_surface_density, get_angular_diameter_distance_a, get_critical_surface_density, predict_tangential_shear, predict_convergence, predict_reduced_tangential_shear
+from .utils import compute_radial_averages, make_bins, convert_units
