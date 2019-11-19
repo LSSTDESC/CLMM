95a48d39a5d22191ae83d19ecb3ef9998fa22cf1
Matthew Ho <mho1@cori04.nersc.gov>
2019-11-19 12:55:34 -0800

    making style changes according to some of Matt's comments

--- clmm/lsst/load_clusters_with_gcr.py
+++ clmm/lsst/load_clusters_with_gcr.py
@@ -1,13 +1,48 @@
 """@file lsst.io.py
 Contains functions to interact with the General Catalog Reader
 """
-from collections import Sequence
+from collections.abc import Sequence
 import os
 import numpy as np
 from astropy.table import Table
 from clmm import GalaxyCluster
 
-def load_from_dc2_rect(N, catalog, save_dir, ra_range=(-0.3, 0.3), dec_range=(-0.3, 0.3),
+def load_catalog(catalog_name):
+    """Loads a catalog from GCRCatalogs"""
+    import GCRCatalogs
+    return GCRCatalogs.load_catalog(catalog_name)
+
+def _get_filters_from_range(filter_name, center, filter_range):
+    """Creates filters for a certain quantity in the GCRCatalogs format. Creates a filter
+    around a center value.
+    
+    Parameters
+    ----------
+    filter_name: str
+        Quantity with which to filter
+    center: float
+        Value about which to construct the filter
+    filter_range: length-2 array-like of floats
+        Range around the center value about which to construct the filter
+    """
+     # checking filter_name type
+    if not isinstance(filter_name, str):
+        raise TypeError('filter_name not string.')
+    # checking for valid filter
+    if len(filter_range) != 2:
+        raise TypeError('%s incorrect length: %s'%(key, len(ranges[key])))
+    if filter_range[0] >= filter_range[1]:
+        raise ValueError('%s invalid range'%key)
+
+    return ['%s >= %d'%(filter_name, center+filter_range[0]),
+            '%s < %d'%(filter_name, center+filter_range[1])]
+
+
+def _calc_reduced_shear(shear, convergence):
+    """Calculates reduced shear from shear and convergence"""
+    return shear/(1-convergence)
+
+def load_from_dc2_rect(nclusters, catalog_name, save_dir, ra_range=(-0.3, 0.3), dec_range=(-0.3, 0.3),
                        z_range=(0.1, 1.5), verbose=False):
     """Load random clusters from DC2 using GCRCatalogs
 
@@ -18,9 +53,9 @@ def load_from_dc2_rect(N, catalog, save_dir, ra_range=(-0.3, 0.3), dec_range=(-0
 
     Parameters
     ----------
-    N: int
+    nclusters: int
         Number of clusters to load and save.
-    catalog: str
+    catalog_name: str
         Name of catalog (without '.yaml')
     save_dir: str
         Path to directory in which to save cluster objects
@@ -33,31 +68,8 @@ def load_from_dc2_rect(N, catalog, save_dir, ra_range=(-0.3, 0.3), dec_range=(-0
     verbose: bool
         Sets the function to print the id of each cluster while loading
     """
-    # check input types and ranges
-    if not isinstance(N, int):
-        raise TypeError('N incorrect type: %s'%type(N))
-    if not isinstance(catalog, str):
-        raise TypeError('catalog incorrect type: %s'%type(catalog))
-    if not isinstance(save_dir, str):
-        raise TypeError('save_dir incorrect type: %s'%type(save_dir))
-    if not isinstance(verbose, bool):
-        raise TypeError('verbose incorrect type: %s'%type(verbose))
 
-    if N <= 0:
-        raise ValueError('N less than 0')
-
-    ranges = {'ra_range': ra_range, 'dec_range': dec_range, 'z_range':z_range}
-    for key in ranges:
-        if not isinstance(ranges[key], Sequence):
-            raise TypeError('%s incorrect type: %s'%(key, type(ranges[key])))
-        if len(ranges[key]) != 2:
-            raise TypeError('%s incorrect length: %s'%(key, len(ranges[key])))
-        for i in ranges[key]:
-            if not isinstance(i, float):
-                raise TypeError('%s value incorrect type: %s'%(key, type(i)))
-        if ranges[key][0] >= ranges[key][1]:
-            raise ValueError('%s invalid range'%key)
-
+    # check that ra, dec are within bounds
     for i in ra_range:
         if not -360. <= i <= 360.:
             raise ValueError(r'ra %s not in valid bounds: [-360, 360]'%i)
@@ -67,17 +79,13 @@ def load_from_dc2_rect(N, catalog, save_dir, ra_range=(-0.3, 0.3), dec_range=(-0
 
 
     # load GCR catalog
-    import GCRCatalogs
-
-    catalog = GCRCatalogs.load_catalog(catalog)
+    catalog = load_catalog(catalog_name)
 
     halos = catalog.get_quantities(['galaxy_id', 'halo_mass', 'redshift', 'ra', 'dec'],
                                    filters=['halo_mass > 1e14', 'is_central==True'])
 
-    if N > len(halos):
-        raise ValueError('N greater than number of clusters available in catalog.')
-
-    for i in np.random.choice(range(len(halos)), N, replace=False):
+    # generate GalaxyCluster objects
+    for i in np.random.choice(range(len(halos)), nclusters, replace=False):
         # specify cluster information
         cl_id = halos['galaxy_id'][i]
         cl_ra = halos['ra'][i]
@@ -88,23 +96,19 @@ def load_from_dc2_rect(N, catalog, save_dir, ra_range=(-0.3, 0.3), dec_range=(-0
             print('Loading cluster %s'%cl_id)
 
         # get galaxies around cluster
-        coord_filters = [
-            'ra >= %s'%(cl_ra+ra_range[0]),
-            'ra < %s'%(cl_ra+ra_range[1]),
-            'dec >= %s'%(cl_dec+dec_range[0]),
-            'dec < %s'%(cl_dec+dec_range[1]),
-        ]
-        z_filters = ['redshift >= %s'%(cl_z+z_range[0]),
-                     'redshift < %s'%(cl_z+z_range[1])]
+        
+        filters = (_get_filters_from_range('ra', cl_ra, ra_range) + 
+                   _get_filters_from_range('dec', cl_dec, dec_range) + 
+                   _get_filters_from_range('redshift', cl_z, z_range))
 
         gals = catalog.get_quantities(['galaxy_id', 'ra', 'dec',
                                        'shear_1', 'shear_2',
                                        'redshift', 'convergence'],
-                                      filters=(coord_filters + z_filters))
+                                      filters=filters)
 
         # calculate reduced shear
-        g1 = gals['shear_1']/(1.-gals['convergence'])
-        g2 = gals['shear_2']/(1.-gals['convergence'])
+        g1 = _calc_reduced_shear(gals['shear_1'], gals['convergence'])
+        g2 = _calc_reduced_shear(gals['shear_2'], gals['convergence'])
 
         # store the results into an astropy table
         t = Table([gals['galaxy_id'], gals['ra'], gals['dec'],
--- tests/test_io.py
+++ tests/test_io.py
@@ -21,7 +21,6 @@ def test_types():
 def test_ranges():
     
     testing.assert_raises(ValueError, clmm.lsst.load_from_dc2_rect, -1, 'cosmoDC2_v1.1.4_small', '.')
-    
     testing.assert_raises(ValueError, clmm.lsst.load_from_dc2_rect, 5, 'cosmoDC2_v1.1.4_small', '.',
                           (0.3, -0.3), (-0.3, 0.3), (0.1, 1.5))
     testing.assert_raises(ValueError, clmm.lsst.load_from_dc2_rect, 5, 'cosmoDC2_v1.1.4_small', '.',
@@ -30,11 +29,3 @@ def test_ranges():
                           (-361., 0.3), (-0.3, 0.3), (0.1, 1.5))
     testing.assert_raises(ValueError, clmm.lsst.load_from_dc2_rect, 5, 'cosmoDC2_v1.1.4_small', '.',
                           (-0.3, 0.3), (-0.3, 95.), (0.1, 1.5))
-    
-    # can't check these because Travis doesn't have access to GCR
-    # testing.assert_raises(ValueError, clmm.lsst.load_from_dc2_rect, 5, 'not_a_catalog', '.')
-    # testing.assert_raises(ValueError, clmm.lsst.load_from_dc2_rect, int(1e20), 'cosmoDC2_v1.1.4_small', '.')
-
-if __name__ == "__main__":
-    test_types()
-    test_ranges()
