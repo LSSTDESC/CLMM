bd0e146607c96025e3e38fc690537de1c33a0005
Michel Aguena <michel.aguena@gmail.com>
2019-11-20 19:17:51 -0300

    add pickled values to tests/test_modeling.py

--- tests/test_modeling.py
+++ tests/test_modeling.py
@@ -10,15 +10,25 @@ import clmm
 # from clmm import modeling as pp
 # from modeling import *
 
-density_profile_parametrization = 'nfw'
-mass_lims = (1.e12, 1.e16)
-mass_Delta = 200
-cluster_mass = 1.e15
-cluster_concentration = 4
-z_cluster = 1.
+import pickle as pkl
+example_case = pkl.load( open( "../support/example_case.p", "rb" ) )
+
+#density_profile_parametrization = 'nfw'
+density_profile_parametrization = example_case['density_profile_parametrization']
+#mass_Delta = 200
+mass_Delta = example_case['mass_Delta']
+#cluster_mass = 1.e15
+cluster_mass = example_case['cluster_mass']
+#cluster_concentration = 4
+cluster_concentration = example_case['cluster_concentration']
+z_max = example_case['z_max']
+#z_cluster = 1.
+z_cluster = example_case['z_cluster']
+z_source = example_case['z_source']
 
 # cosmo_ccl = ccl.Cosmology(Omega_c=0.27, Omega_b=0.045, h=0.67, A_s=2.1e-9, n_s=0.96)
-cosmo_apy= astropy.cosmology.core.FlatLambdaCDM(H0=70, Om0=0.27, Ob0=0.045)
+#cosmo_apy= astropy.cosmology.core.FlatLambdaCDM(H0=70, Om0=0.27, Ob0=0.045)
+cosmo_apy = example_case['astropy_cosmology_object']
 cosmo_ccl = clmm.cclify_astropy_cosmo(cosmo_apy)
 
 def test_cosmo_type():
@@ -26,14 +36,18 @@ def test_cosmo_type():
     tst.assert_equal(type(cosmo_ccl), dict)
     tst.assert_equal(cosmo_ccl['Omega_c'] + cosmo_ccl['Omega_b'], cosmo_apy.Odm0 + cosmo_apy.Ob0)
 
-r3d = np.logspace(-2, 2, 100)
+#r3d = np.logspace(-2, 2, 100)
+r3d = example_case['r3d']
 r3d_one = r3d[-1]
 
 rho = clmm.get_3d_density(r3d, mdelta=cluster_mass, cdelta=cluster_concentration, z_cl=z_cluster, cosmo=cosmo_ccl)
 rho_one = clmm.get_3d_density(r3d_one, mdelta=cluster_mass, cdelta=cluster_concentration, z_cl=z_cluster, cosmo=cosmo_ccl)
 
 def test_rho():
+    # consistency test
     tst.assert_equal(rho[-1], rho_one)
+    # physical value test
+    tst.assert_allclose(example_case['nc_rho'], rho, 1e-8)
 
 Sigma = clmm.predict_surface_density(r3d, cluster_mass, cluster_concentration, z_cl=z_cluster, cosmo=cosmo_ccl, delta_mdef=200, halo_profile_model='nfw')
 Sigma_one = clmm.predict_surface_density(r3d_one, cluster_mass, cluster_concentration, z_cl=z_cluster, cosmo=cosmo_ccl, delta_mdef=200, halo_profile_model='nfw')
