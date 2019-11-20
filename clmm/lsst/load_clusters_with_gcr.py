262959bc351b5f358a0d526414fa572e7b9ef328
Matthew Ho <mho1@cori07.nersc.gov>
2019-11-19 20:18:03 -0800

    some style changes. added a _test_catalog to spoof a GCRCatalogs object to be used for travis testing. Still haven't made a reduced shear calculator in a science module yet

--- clmm/lsst/load_clusters_with_gcr.py
+++ clmm/lsst/load_clusters_with_gcr.py
@@ -7,43 +7,45 @@ import numpy as np
 from astropy.table import Table
 from clmm import GalaxyCluster
 
-def load_catalog(catalog_name):
+def load_GCR_catalog(catalog_name):
     """Loads a catalog from GCRCatalogs"""
     import GCRCatalogs
     return GCRCatalogs.load_catalog(catalog_name)
 
-def _get_filters_from_range(filter_name, center, filter_range):
-    """Creates filters for a certain quantity in the GCRCatalogs format. Creates a filter
-    around a center value.
+class _test_catalog():
+    """Blank test catalog designed to mimic a GCRCatalogs catalog. For testing only."""
+    def __len__(self):
+        return 10
+    def get_quantities(self, quantities, *args, **kwargs):
+        return dict([(q, np.zeros(10)) for q in quantities])
+
+def _make_GCR_filter(filter_name, low_bound, high_bound):
+    """Create a filter for a specified range of a certain quantity in the GCRCatalogs format.
     
     Parameters
     ----------
     filter_name: str
-        Quantity with which to filter
-    center: float
-        Value about which to construct the filter
-    filter_range: length-2 array-like of floats
-        Range around the center value about which to construct the filter
+        Quantity to filter
+    low_bound, high_bound: float
+        Range of values that the quantity is restricted to
     """
      # checking filter_name type
     if not isinstance(filter_name, str):
         raise TypeError('filter_name not string.')
     # checking for valid filter
-    if len(filter_range) != 2:
-        raise TypeError('%s incorrect length: %s'%(key, len(ranges[key])))
-    if filter_range[0] >= filter_range[1]:
-        raise ValueError('%s invalid range'%key)
+    if low_bound >= high_bound:
+        raise ValueError('Invalid range: [%d, %d]'%(low_bound, high_bound))
 
-    return ['%s >= %d'%(filter_name, center+filter_range[0]),
-            '%s < %d'%(filter_name, center+filter_range[1])]
+    return ['%s >= %d'%(filter_name, low_bound),
+            '%s < %d'%(filter_name, high_bound)]
 
 
 def _calc_reduced_shear(shear, convergence):
     """Calculates reduced shear from shear and convergence"""
     return shear/(1-convergence)
 
-def load_from_dc2_rect(nclusters, catalog_name, save_dir, ra_range=(-0.3, 0.3), dec_range=(-0.3, 0.3),
-                       z_range=(0.1, 1.5), verbose=False):
+def load_from_dc2(nclusters, catalog_name, save_dir, ra_range=(-0.3, 0.3), dec_range=(-0.3, 0.3),
+                  z_range=(0.1, 1.5), verbose=False, _reader='GCR'):
     """Load random clusters from DC2 using GCRCatalogs
 
     This function saves a set of random clusters loaded from a DC2 catalog. The galaxies
@@ -60,32 +62,38 @@ def load_from_dc2_rect(nclusters, catalog_name, save_dir, ra_range=(-0.3, 0.3), 
     save_dir: str
         Path to directory in which to save cluster objects
     ra_range: length-2 array-like of floats, optional
-        Range of right ascension values (in degrees) to cut galaxies around the cluster center
+        Range of right ascension values (in degrees) to cut galaxies relative to the cluster center
     dec_range: length-2 array-like of floats, optional
-        Range of declination values (in degrees)to cut galaxies around the cluster center
+        Range of declination values (in degrees)to cut galaxies relative to the cluster center
     z_range: length-2 array-like of floats, optional
-        Range of redshift values to cut galaxies around the cluster center
+        Range of redshift values to cut galaxies relative to the cluster center
     verbose: bool
         Sets the function to print the id of each cluster while loading
+    _reader: str
+        Reader argument used for testing. In practice, should be default to 'GCR'.
     """
+    # check that ranges are length-2
+    if len(ra_range)!=2:
+        raise ValueError('ra_range incorrect length: %i'%len(ra_range))
+    if len(dec_range)!=2:
+        raise ValueError('dec_range incorrect length: %i'%len(dec_range))
+    if len(z_range)!=2:
+        raise ValueError('z_range incorrect length: %i'%len(z_range))
 
-    # check that ra, dec are within bounds
-    for i in ra_range:
-        if not -360. <= i <= 360.:
-            raise ValueError(r'ra %s not in valid bounds: [-360, 360]'%i)
-    for i in dec_range:
-        if not -90. <= i <= 90.:
-            raise ValueError(r'dec %s not in valid bounds: [-90, 90]'%i)
 
-
-    # load GCR catalog
-    catalog = load_catalog(catalog_name)
+    # load catalog
+    if _reader=='GCR':
+        catalog = load_GCR_catalog(catalog_name)
+    elif _reader=='test':
+        catalog = _test_catalog()
+    else:
+        raise ValueError('Invalid reader name: %s'%_reader)
 
     halos = catalog.get_quantities(['galaxy_id', 'halo_mass', 'redshift', 'ra', 'dec'],
                                    filters=['halo_mass > 1e14', 'is_central==True'])
 
     # generate GalaxyCluster objects
-    for i in np.random.choice(range(len(halos)), nclusters, replace=False):
+    for i in np.random.choice(range(len(next(iter(halos)))), nclusters, replace=False):
         # specify cluster information
         cl_id = halos['galaxy_id'][i]
         cl_ra = halos['ra'][i]
@@ -97,9 +105,9 @@ def load_from_dc2_rect(nclusters, catalog_name, save_dir, ra_range=(-0.3, 0.3), 
 
         # get galaxies around cluster
         
-        filters = (_get_filters_from_range('ra', cl_ra, ra_range) + 
-                   _get_filters_from_range('dec', cl_dec, dec_range) + 
-                   _get_filters_from_range('redshift', cl_z, z_range))
+        filters = (_make_GCR_filter('ra', cl_ra + ra_range[0], cl_ra + ra_range[1]) + 
+                   _make_GCR_filter('dec', cl_dec + dec_range[0], cl_dec + dec_range[1]) + 
+                   _make_GCR_filter('redshift', cl_z + z_range[0], cl_z + z_range[1]))
 
         gals = catalog.get_quantities(['galaxy_id', 'ra', 'dec',
                                        'shear_1', 'shear_2',
--- tests/test_io.py
+++ tests/test_io.py
@@ -6,26 +6,25 @@ from numpy import testing
 import clmm
 
 def test_types():
-    testing.assert_raises(TypeError, clmm.lsst.load_from_dc2_rect, 1.5, 'cosmoDC2_v1.1.4_small', '.')
-    testing.assert_raises(TypeError, clmm.lsst.load_from_dc2_rect, 5, 10, '.')
-    testing.assert_raises(TypeError, clmm.lsst.load_from_dc2_rect, 5, 'cosmoDC2_v1.1.4_small', 10)
+    testing.assert_raises(TypeError, clmm.lsst.load_from_dc2, 1.5, 'cosmoDC2_v1.1.4_small',
+                          '.', _reader='test')
+    testing.assert_raises(TypeError, clmm.lsst.load_from_dc2, 5, 10, '.', _reader='test')
+    testing.assert_raises(TypeError, clmm.lsst.load_from_dc2, 5, 'cosmoDC2_v1.1.4_small',
+                          10, _reader='test')
     
-    testing.assert_raises(TypeError, clmm.lsst.load_from_dc2_rect, 5, 'cosmoDC2_v1.1.4_small', '.',
-                          (-5, 0.3), (-0.3, 0.3), (0.1, 1.5))
-    testing.assert_raises(TypeError, clmm.lsst.load_from_dc2_rect, 5, 'cosmoDC2_v1.1.4_small', '.',
-                          (-0.3, 0.3), (-0.3, 0.3, 0.1), (0.1, 1.5))
+    testing.assert_raises(TypeError, clmm.lsst.load_from_dc2, 5, 'cosmoDC2_v1.1.4_small', '.',
+                          (-5, 0.3), (-0.3, 0.3), (0.1, 1.5), _reader='test')
+    testing.assert_raises(TypeError, clmm.lsst.load_from_dc2, 5, 'cosmoDC2_v1.1.4_small', '.',
+                          (-0.3, 0.3), (-0.3, 0.3, 0.1), (0.1, 1.5), _reader='test')
     
-    testing.assert_raises(TypeError, clmm.lsst.load_from_dc2_rect, 5, 'cosmoDC2_v1.1.4_small', '.',
-                          (-0.3, 0.3), (-0.3, 0.3), (0.1, 1.5), verbose=1.3)
+    testing.assert_raises(TypeError, clmm.lsst.load_from_dc2, 5, 'cosmoDC2_v1.1.4_small', '.',
+                          (-0.3, 0.3), (-0.3, 0.3), (0.1, 1.5), verbose=1.3, _reader='test')
     
 def test_ranges():
     
-    testing.assert_raises(ValueError, clmm.lsst.load_from_dc2_rect, -1, 'cosmoDC2_v1.1.4_small', '.')
-    testing.assert_raises(ValueError, clmm.lsst.load_from_dc2_rect, 5, 'cosmoDC2_v1.1.4_small', '.',
-                          (0.3, -0.3), (-0.3, 0.3), (0.1, 1.5))
-    testing.assert_raises(ValueError, clmm.lsst.load_from_dc2_rect, 5, 'cosmoDC2_v1.1.4_small', '.',
-                          (0.3, -0.3), (-0.3, 0.3), (0.1, 1.5))
-    testing.assert_raises(ValueError, clmm.lsst.load_from_dc2_rect, 5, 'cosmoDC2_v1.1.4_small', '.',
-                          (-361., 0.3), (-0.3, 0.3), (0.1, 1.5))
-    testing.assert_raises(ValueError, clmm.lsst.load_from_dc2_rect, 5, 'cosmoDC2_v1.1.4_small', '.',
-                          (-0.3, 0.3), (-0.3, 95.), (0.1, 1.5))
+    testing.assert_raises(ValueError, clmm.lsst.load_from_dc2, -1, 'cosmoDC2_v1.1.4_small', '.',
+                          _reader='test')
+    testing.assert_raises(ValueError, clmm.lsst.load_from_dc2, int(1e20), 'cosmoDC2_v1.1.4_small', '.',
+                          _reader='test')
+    testing.assert_raises(ValueError, clmm.lsst.load_from_dc2, 5, 'cosmoDC2_v1.1.4_small', '.',
+                          (0.3, -0.3), (-0.3, 0.3), (0.1, 1.5), _reader='test')
