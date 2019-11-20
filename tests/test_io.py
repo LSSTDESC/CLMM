e3634f1fbfeec0e0b645a93afc91388d8ea5ae92
Matthew Ho <mho1@cori07.nersc.gov>
2019-11-19 20:30:32 -0800

    wrong error type

--- tests/test_io.py
+++ tests/test_io.py
@@ -11,9 +11,6 @@ def test_types():
     testing.assert_raises(TypeError, clmm.lsst.load_from_dc2, 5, 'cosmoDC2_v1.1.4_small',
                           10, _reader='test')
     testing.assert_raises(TypeError, clmm.lsst.load_from_dc2, 5, 'cosmoDC2_v1.1.4_small', '.',
-                          (-0.3, 0.3), (-0.3, 0.3, 0.1), (0.1, 1.5), _reader='test')
-    
-    testing.assert_raises(TypeError, clmm.lsst.load_from_dc2, 5, 'cosmoDC2_v1.1.4_small', '.',
                           (-0.3, 0.3), (-0.3, 0.3), (0.1, 1.5), verbose=1.3, _reader='test')
     
 def test_ranges():
@@ -22,5 +19,7 @@ def test_ranges():
                           _reader='test')
     testing.assert_raises(ValueError, clmm.lsst.load_from_dc2, int(1e10), 'cosmoDC2_v1.1.4_small', '.',
                           _reader='test')
+    testing.assert_raises(ValueError, clmm.lsst.load_from_dc2, 5, 'cosmoDC2_v1.1.4_small', '.',
+                          (-0.3, 0.3), (-0.3, 0.3, 0.1), (0.1, 1.5), _reader='test')
     testing.assert_raises(ValueError, clmm.lsst.load_from_dc2, 5, 'cosmoDC2_v1.1.4_small', '.',
                           (0.3, -0.3), (-0.3, 0.3), (0.1, 1.5), _reader='test')
