0460b8f9a8a98701012a2e3a59269b432f755ae4
matthewkirby <matthewkirby@email.arizona.edu>
2019-11-21 15:37:12 -0600

    Resolve the Pygments ipython3 error in docs

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,8 @@ import subprocess
 
 # -- General configuration ------------------------------------------------
 extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.napoleon']
+              'sphinx.ext.napoleon',
+              'IPython.sphinxext.ipython_console_highlighting']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
