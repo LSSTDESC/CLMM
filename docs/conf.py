7fd891133749d30b5d0aaad24c05434877888e96
matthewkirby <matthewkirby@email.arizona.edu>
2019-11-22 11:00:45 -0600

    another path

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import sys
 sys.path.insert(0, os.path.abspath('../clmm'))
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- General configuration ------------------------------------------------
