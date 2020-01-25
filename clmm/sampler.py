83d331b8ccb4cc59a150f4626d05ec4ccee1cce3
Camille Avestruz <briarrose5187@gmail.com>
2020-01-24 16:13:18 -0800

    added example for scipy optimize minimize

--- clmm/sampler.py
+++ clmm/sampler.py
@@ -1,4 +1,20 @@
 """@file.py sampler.py
-Functions for sampling
+Functions for sampling (output either peak or full distribution)
 
 """
+
+def sciopt() :
+    ''' Uses scipy optimize minimize to output the peak'''
+    from scipy import optimize as spo
+
+    # spo.minimize(nfw_to_shear_profile, logm_0,
+    #                     args=[r1, gt_profile1, z1]).x
+
+    pass
+
+
+
+samplers = {
+    'minimize':sciopt,
+    
+    }
