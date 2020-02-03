583ff21aed86e66036d7cfe1aace7a2257c313ff
Ricardo Herbonnet <rherbonn@cori06.nersc.gov>
2020-02-03 13:19:43 -0800

    some general naming of variables

--- examples/support/sampler.py
+++ examples/support/sampler.py
@@ -3,12 +3,12 @@ Functions for sampling (output either peak or full distribution)
 
 """
 
-def sciopt(nfw_to_shear_profile, logm_0, args) :
+def sciopt(model_to_shear_profile, logm_0, args) :
     ''' Uses scipy optimize minimize to output the peak'''
     from scipy import optimize as spo
 
-    return spo.minimize(nfw_to_shear_profile, logm_0,
-                 args=[r1, gt_profile1, z1]).x
+    return spo.minimize(model_to_shear_profile, logm_0,
+                 args=args).x
 
 
 
