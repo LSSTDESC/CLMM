f0458213b041973e1159bdca12498012110e892c
Camille Avestruz <briarrose5187@gmail.com>
2020-02-03 13:02:15 -0800

    Added return value in sampler.py that matches the notebook.

--- examples/support/sampler.py
+++ examples/support/sampler.py
@@ -7,10 +7,8 @@ def sciopt(nfw_to_shear_profile, logm_0, args) :
     ''' Uses scipy optimize minimize to output the peak'''
     from scipy import optimize as spo
 
-    spo.minimize(nfw_to_shear_profile, logm_0,
+    return spo.minimize(nfw_to_shear_profile, logm_0,
                  args=[r1, gt_profile1, z1]).x
-
-    return logm_0
 
 
 
