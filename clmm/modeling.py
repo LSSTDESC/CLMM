e57dd12cc05e126ee3ea88838a907ed87740f2ea
rherbonnet <rherbonnet@gmail.com>
2020-04-17 13:32:13 -0700

    Added functionality for a float R_proj, as well as an array

--- clmm/modeling.py
+++ clmm/modeling.py
@@ -271,7 +271,7 @@ def predict_excess_surface_density(r_proj, mdelta, cdelta, z_cl, cosmo, delta_md
         raise ValueError(f"Rmin = {np.min(r_proj):.2e} Mpc/h! This value is too small and may cause computational issues.")
 
     # Computing sigma on a larger range than the radial range requested
-    sigma_r_proj = np.logspace(np.log10(np.min(r_proj))-1, np.log10(np.max(r_proj))+1, len(r_proj)*10)
+    sigma_r_proj = np.logspace(np.log10(np.min(r_proj))-1, np.log10(np.max(r_proj))+1, 10*np.array(r_proj).size)
 
     if halo_profile_model.lower() == 'nfw':
         sigma = ct.deltasigma.Sigma_nfw_at_R(sigma_r_proj, mdelta, cdelta,
