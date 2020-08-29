6396b204a13ba3983165d932735333085994431d
Sandro Dias Pinto Vitenti <sandro@isoftware.com.br>
2020-08-29 14:45:16 -0300

    Removed unnecessary verbosity and added tests for invalid backend.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,17 @@ import importlib
 import clmm
 import os
 
-@pytest.fixture (scope = "module", params = ["ct", "nc", "ccl"])
+@pytest.fixture (scope = "module", params = ["ct", "nc", "ccl", "testnotabackend"])
 def modeling_data (request):
     param = request.param
-
-    if (not clmm.modeling.backend_is_available (param)):
+    
+    try:
+        avail = clmm.modeling.backend_is_available (param)
+    except ValueError:
+        avail = False
+        
+    
+    if (not avail):
         pytest.skip (f"Unsupported backend `{param}'.")
 
     os.environ['CLMM_MODELING_BACKEND'] = param
