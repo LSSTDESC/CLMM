e595b4c8fc252f5bb45905e8a615ecd412878ac6
Michel Aguena <aguena@lapp.in2p3.fr>
2020-10-14 17:54:00 +0200

    add Cosmology directly to clmm

--- clmm/__init__.py
+++ clmm/__init__.py
@@ -3,7 +3,7 @@ from .gcdata import GCData
 from .galaxycluster import GalaxyCluster
 from .polaraveraging import compute_tangential_and_cross_components, make_binned_profile
 from .utils import compute_radial_averages, make_bins, convert_units
-from .modeling import get_reduced_shear_from_convergence, get_3d_density, predict_surface_density, predict_excess_surface_density, angular_diameter_dist_a1a2, get_critical_surface_density, predict_tangential_shear, predict_convergence, predict_reduced_tangential_shear, Modeling
+from .modeling import get_reduced_shear_from_convergence, get_3d_density, predict_surface_density, predict_excess_surface_density, angular_diameter_dist_a1a2, get_critical_surface_density, predict_tangential_shear, predict_convergence, predict_reduced_tangential_shear, Modeling, Cosmology
 
 from . import lsst
 
