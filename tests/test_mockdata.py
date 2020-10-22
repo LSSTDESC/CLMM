e3137dd286f20a2644e87ec8c25d977bda3a1aaf
rherbonnet <rherbonnet@gmail.com>
2020-10-22 08:06:27 -0700

    new validation test for mock_data.py (#303)

--- tests/test_mockdata.py
+++ tests/test_mockdata.py
@@ -1,18 +1,14 @@
 """Tests for examples/support/mock_data.py"""
 import numpy as np
-from numpy.testing import assert_raises, assert_allclose, assert_equal
-from astropy.cosmology import FlatLambdaCDM, LambdaCDM
+from numpy.testing import assert_allclose, assert_equal
+from astropy.cosmology import FlatLambdaCDM
 import clmm
 import clmm.polaraveraging as pa
 import sys
 sys.path.append('examples/support')
-sys.path.append('../examples/support')
 import mock_data as mock
-from sampler import samplers
 from sampler import fitters
-import matplotlib.pyplot as plt
 
-#TOLERANCE = {'rtol': 1.0e-6, 'atol': 1.0e-6}
 TOLERANCE = {'rtol': 5.0e-4, 'atol': 1.e-4}
 
 
@@ -31,12 +27,13 @@ def test_mock_data():
         return gt_model
 
     def mass_mock_cluster(mass=15.,guess=15.):
+        
         # Set up mock cluster
-        data = mock.generate_galaxy_catalog(10**mass, 0.3, 4, cosmo, 200, 0.8, ngals=100000)
-        #print(data.keys())
+        ngals=50000
+        data = mock.generate_galaxy_catalog(10**mass, 0.3, 4, cosmo, 200, 0.8, ngals=ngals)
         
         # Check whether the given ngals is the retrieved ngals
-        assert_equal(len(data['ra']),100000)
+        assert_equal(len(data['ra']),ngals)
         
         # Check that there are no galaxies with |e|>1
         assert_equal(np.count_nonzero((data['e1']>1) | (data['e1']<-1)),0)
@@ -45,7 +42,7 @@ def test_mock_data():
         # Create shear profile
         cl = clmm.GalaxyCluster("test_cluster", 0.0, 0.0, 0.3, data)
         theta, g_t, g_x = pa.compute_tangential_and_cross_components(cl, geometry="flat")
-        binned = pa.make_binned_profile(cl, "radians", "Mpc", bins=pa.make_bins(0.5, 5.0, 200), 
+        binned = pa.make_binned_profile(cl, "radians", "Mpc", bins=pa.make_bins(0.5, 5.0, 100), 
                                   cosmo=cosmo, include_empty_bins=False)
 
         popt,pcov = fitters['curve_fit'](lambda r, logm: nfw_shear_profile(r, logm, 0.8), 
@@ -57,10 +54,11 @@ def test_mock_data():
         return popt[0]
     
     
-    input_masses = np.random.uniform(13.5, 16., 50)
-    logm_0 = np.random.uniform(13.5, 16., 50)
-    
-    meas_masses = np.array([mass_mock_cluster(m,g) for m,g in zip(input_masses,logm_0)])
+    #input_masses = np.random.uniform(13.5, 16., 25)
+    #logm_0 = np.random.uniform(13.5, 16., 25)
+    input_masses = np.array([13.82,15.94,13.97,14.17,14.95,15.49,14.93,15.59])
+    guess_masses = np.array([15.70,15.10,15.08,14.31,13.61,13.90,15.07,13.82])
+    meas_masses = [mass_mock_cluster(m,g) for m,g in zip(input_masses,guess_masses)]
     assert_allclose(meas_masses,input_masses, **TOLERANCE)
 
     
