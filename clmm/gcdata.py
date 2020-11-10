632834cf1fa051b0639fdb4d19372ca226312d42
hsin <57552401+hsinfan1996@users.noreply.github.com>
2020-11-10 20:38:50 +0800

    Issue/314/cosmology consistency (#317)
    * add valid_cosmo func
    
    * fix change in compute_tangential_and_cross_components
    
    * add import in utils
    
    * update conditions on valid_cosmo
    
    * add updata_cosmo in GCData
    
    * add test_valid_cosmo
    
    * mv valid_cosmo functionality to Cosmology.update_cosmo_ext_valid
    
    * update variable name in update_cosmo_ext_valid
    
    * update docstring in update_cosmo
    
    * add GCMetaData class to prevent cosmo to be updated manually
    
    * update version number
    
    Co-authored-by: Michel Aguena <aguena@lapp.in2p3.fr>
    Co-authored-by: m-aguena <aguena@if.usp.br>
    Co-authored-by: combet <celine.combet@lpsc.in2p3.fr>
    
    trying again to fix conflict

---
+++ clmm/gcdata.py
@@ -0,0 +1,194 @@
+"""
+Define the custom data type
+"""
+from astropy.table import Table as APtable
+import warnings
+import pickle
+
+from collections import OrderedDict
+class GCMetaData(OrderedDict):
+    r"""Object to store metadata, it always has a cosmo key with protective changes
+
+    Attributes
+    ----------
+    protected: bool
+        Protect cosmo key
+    OrderedDict attributes
+    """
+    def __init__(self, *args, **kwargs):
+        OrderedDict.__init__(self, *args, **kwargs)
+        if 'cosmo' not in self:
+            self.__setitem__('cosmo', None, True)
+    def __setitem__(self, item, value, force=False):
+        if item == 'cosmo' and not force and \
+            (self['cosmo'] is not None if 'cosmo' in self else False):
+            raise ValueError('cosmo must be changed via update_cosmo or update_cosmo_ext_valid method')
+        else:
+            OrderedDict.__setitem__(self, item, value)
+        return
+class GCData(APtable):
+    """
+    GCData: A data objetc for gcdata. Right now it behaves as an astropy table.
+
+    Parameters
+    ----------
+    meta: dict
+        Dictionary with metadata for this object
+
+    Same as astropy tables
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        Parameters
+        ----------
+        *args, **kwargs: Same used for astropy tables
+        """
+        APtable.__init__(self, *args, **kwargs)
+        metakwargs = kwargs['meta'] if 'meta' in kwargs else {}
+        metawkargs = {} if metakwargs is None else metakwargs
+        self.meta = GCMetaData(**metakwargs)
+    def __repr__(self):
+        """Generates string for repr(GCData)"""
+        output = f'{self.__class__.__name__}('
+        output+= ', '.join([f'{key}={value!r}'
+            for key, value in self.meta.items()]
+                +['columns: '+', '.join(self.colnames)])
+        output+= ')'
+        return output
+    def __str__(self):
+        """Generates string for print(GCData)"""
+        output = f'self.__class__.__name__\n> defined by:'
+        output+= ', '.join([f'{key}={str(value)}'
+            for key, value in self.meta.items()])
+        output+= f'\n> with columns: '
+        output+= ', '.join(self.colnames)
+        return output
+    def __getitem__(self, item):
+        """
+        Makes sure GCData keeps its properties after [] operations are used
+
+        Returns
+        -------
+        GCData
+            Data with [] operations applied
+        """
+        out = APtable.__getitem__(self, item)
+        return out
+    def update_cosmo_ext_valid(self, gcdata, cosmo, overwrite=False):
+        r"""Updates cosmo metadata if the same as in gcdata
+
+        Parameters
+        ----------
+        gcdata: GCData
+            Table to check if same cosmology
+        cosmo: clmm.Cosmology
+            Cosmology
+        overwrite: bool
+            Overwrites the current cosmo metadata. If false raises Error when cosmologies are different.
+
+        Returns
+        -------
+        None
+        """
+        cosmo_desc = cosmo.get_desc() if cosmo else None
+        if cosmo_desc:
+            cosmo_gcdata = gcdata.meta['cosmo']
+            if cosmo_gcdata and cosmo_gcdata != cosmo_desc:
+                if overwrite:
+                    warnings.warn(f'input cosmo ({cosmo_desc}) overwriting gcdata cosmo ({cosmo_gcdata})')
+                else:
+                    raise TypeError(f'input cosmo ({cosmo_desc}) differs from gcdata cosmo ({cosmo_gcdata})')
+            self.meta.__setitem__('cosmo', cosmo_desc, force=True)
+        return
+    def update_cosmo(self, cosmo, overwrite=False):
+        r"""Updates cosmo metadata if not present
+
+        Parameters
+        ----------
+        cosmo: clmm.Cosmology
+            Cosmology
+        overwrite: bool
+            Overwrites the current cosmo metadata. If false raises Error when cosmologies are different.
+
+        Returns
+        -------
+        None
+        """
+        self.update_cosmo_ext_valid(self, cosmo, overwrite=overwrite)
+        return
+
+"""
+Additional functions specific to clmm.GCData
+Note: Not being used anymore
+"""
+#
+#def confirm_GCData(data):
+#    """
+#    Typechecks GCData elements
+#    """
+#    # if not (type(data.creator) == str and type(data.specs) == dict and type(data.values) == astropy.Table):
+#    #     raise TypeError('GCData creator should be string, specs should be dict, values should be astropy table')
+#    pass
+#
+#
+#def read_GCData(filename):
+#    """
+#    Reads GCData from file
+#    """
+#    pass
+#
+#
+#def write_GCData(data, filename):
+#    """
+#    Writes GCData to file
+#    """
+#    pass
+#
+#
+#def check_subdict(lookup_dict, reference_dict):
+#    '''
+#    Checks if a dictionary is a subset of another dictionary, with the same keys and values
+#
+#    Parameters
+#    ----------
+#    lookup_dict: dict
+#        Subset dictionary
+#    reference_dict: dict
+#        Main dictionary
+#
+#    Returns
+#    -------
+#    bool
+#        If a lookup_dict is a subset of reference_dict
+#    '''
+#    return lookup_dict.items() <= reference_dict.items()
+#
+#
+#def find_in_datalist(lookup_specs, datalist, exact=False):
+#    '''
+#    Finds data with given specs in a datalist, allows for partial match
+#
+#    Parameters
+#    ----------
+#    datalist: list
+#        List of clmm.GCData objects to search for lookup_specs
+#    lookup_specs: dict
+#        Specs required
+#    exact: boolean, optional
+#        Does the match have to be symmetric?
+#
+#    Returns
+#    -------
+#    found: list
+#        List of clmm.GCData object data with required creator and set of specs
+#    '''
+#    found = []
+#    for data in datalist:
+#        if check_subdict(lookup_specs, data.specs) :
+#            found.append(data)
+#    if exact:
+#        for match in found:
+#            if check_subdict(match.specs, lookup_specs):
+#                return [match]
+#        found = []
+#    return found
