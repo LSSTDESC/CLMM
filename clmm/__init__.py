a22e9d83c13cfa11ab28154eae10c43c3e45c145
Michel Aguena <michel.aguena@gmail.com>
2020-12-15 16:19:36 +0100

    re add plotting to galaxycluster (#346)
    feat: make plot_profile a method of the GalaxyCluster object
    
    * re add plotting to galaxycluster
    
    * add plot_profiles into GalaxyCluster object
    
    * update beahavior of missing col in GalaxyCluster.plot_profiles
    
    * update use of plot_profiles in demo_dataops_functionality
    
    * update curve labels on plot_profiles
    
    * fix y label of plot_profiles
    
    Co-authored-by: m-aguena <aguena@if.usp.br>

--- clmm/__init__.py
+++ clmm/__init__.py
@@ -4,6 +4,7 @@ from .galaxycluster import GalaxyCluster
 from .dataops import compute_tangential_and_cross_components, make_radial_profile
 from .utils import compute_radial_averages, make_bins, convert_units
 from .modeling import get_reduced_shear_from_convergence, get_3d_density, predict_surface_density, predict_excess_surface_density, get_critical_surface_density, predict_tangential_shear, predict_convergence, predict_reduced_tangential_shear, Modeling, Cosmology
+from .plotting import plot_profiles
 
 from . import lsst
 
--- clmm/plotting.py
+++ clmm/plotting.py
@@ -1,38 +1,29 @@
 """A collection of scripts that can be used to plot the various quantities that CLMM models."""
 import matplotlib.pyplot as plt
-from .galaxycluster import GalaxyCluster
+import warnings
 
 
-def plot_profiles(cluster=None, rbins=None, tangential_component=None, tangential_component_error=None,
-                  cross_component=None, cross_component_error=None, r_units=None, table_name='profile',
-                  xscale='linear',yscale='linear'):
+def plot_profiles(rbins, tangential_component, tangential_component_error,
+                  cross_component, cross_component_error, r_units=None,
+                  xscale='linear', yscale='linear',
+                  tangential_component_label="Tangential component",
+                  cross_component_label="Cross component"):
     """Plot shear profiles
-
-    This function can be called by either passing in an instance of `GalaxyCluster` or as an
-    attribute of an instance of a `GalaxyCluster` object assuming that that instance has had
-    a shear profile computed and saved as a `.profile` attribute. This function can also be
-    called by passing in `rbins` along with the respective shears.
-
-    We require at least `rbins` information and `tangential_component` information.
 
     Parameters
     ----------
-    cluster: GalaxyCluster, optional
-        Instance of `GalaxyCluster()` that contains a `.profile` attribute.
-    rbins: array_like, optional
+    rbins: array_like
         The centers of the radial bins that was used to compute the shears.
-    tangential_component: array_like, optional
-        The tangential component at the radii of `rbins`, or the name of the column in the galcat Table corresponding to the tangential component of the shear or reduced shear (Delta Sigma not yet implemented). Default: 'gt'
-    tangential_component_error: array_like, optional
-        The uncertainty in the tangential component or the name of the column in the galcat Table corresponding to the uncertainty in tangential component of the shear or reduced shear. Default: 'gt_err'
-    cross_component: array_like, optional
-        The cross component at the radii of `rbins` or the name of the column in the galcat Table corresponding to the cross component of the shear or reduced shear. Default: 'gx'
-    cross_component_error: array_like, optional
-        The uncertainty in the cross component or the name of the column in the galcat Table corresponding to the uncertainty in the cross component of the shear or reduced shear. Default: 'gx_err'
-    r_units: str, optional
+    tangential_component: array_like
+        The tangential component at the radii of `rbins`
+    tangential_component_error: array_like
+        The uncertainty in the tangential component
+    cross_component: array_like
+        The cross component at the radii of `rbins`
+    cross_component_error: array_like
+        The uncertainty in the cross component
+    r_units: str
         Units of `rbins` for x-axis label
-    table_name: str, optional
-        Name of the GalaxyCluster() `.profile` attribute. Default: 'profile'
     xscale:
         matplotlib.pyplot.xscale parameter to set x-axis scale (e.g. to logarithmic axis)
     yscale:
@@ -45,66 +36,22 @@ def plot_profiles(cluster=None, rbins=None, tangential_component=None, tangentia
     axes:
         The matplotlib axes object that has been plotted to.
     """
-
-    # If a cluster object was passed, use these arrays
-    if cluster is not None and hasattr(cluster, table_name):
-        cluster_profile = getattr(cluster,table_name)
-        rbins = cluster_profile['radius']
-        r_units = cluster_profile.meta['bin_units']
-        if tangential_component != 'gt':
-            ValueError("The function requires a column called 'gt' to run.")
-        if cross_component != 'gx':
-            ValueError("The function requires a column called 'gx' to run.")
-        if 'gt' not in cluster_profile.colnames:
-            ValueError("The function requires a column called 'gt' to run.")
-        if 'gx' not in cluster_profile.colnames:
-            ValueError("The function requires a column called 'gx' to run.")
-        if type(tangential_component)==str:
-            tangential_component = cluster_profile[tangential_component]
-        else:
-            tangential_component = cluster_profile['gt']
-        try:
-            if type(tangential_component_error)==str:
-                tangential_component_error = cluster_profile[tangential_component_error]
-            else:
-                tangential_component_error = cluster_profile['gt_err']
-        except:
-            pass
-        try:
-            if type(cross_component)==str:
-                cross_component = cluster_profile[cross_component]
-            else:
-                cross_component = cluster_profile['gx']
-        except:
-            pass
-        try:
-            if type(cross_component_error)==str:
-                cross_component_error = cluster_profile[cross_component_error]
-            else:
-                cross_component_error = cluster_profile['gx_err']
-        except:
-            pass
-
     # Plot the tangential shears
     fig, axes = plt.subplots()
     axes.errorbar(rbins, tangential_component,
                   yerr=tangential_component_error,
-                  fmt='bo-', label="Tangential component")
-
+                  fmt='bo-', label=tangential_component_label,
+                  lw=.2, markersize=1.5, elinewidth=.5, capthick=3)
     # Plot the cross shears
-    try:
-        axes.errorbar(rbins, cross_component,
-                 yerr=cross_component_error,
-                 fmt='ro-', label="Cross component")
-    except:
-        pass
-
+    axes.errorbar(rbins, cross_component,
+                  yerr=cross_component_error,
+                  fmt='ro-', label=cross_component_label,
+                  lw=.2, markersize=1.5, elinewidth=.5, capthick=3)
+    # format
     axes.set_xscale(xscale)
     axes.set_yscale(yscale)
     axes.legend()
     axes.set_xlabel(f'Radius [{r_units}]')
-    axes.set_ylabel(r'$\gamma$')
+    axes.set_ylabel(rf'{tangential_component_label} | {cross_component_label}')
 
     return fig, axes
-
-GalaxyCluster.plot_profiles = plot_profiles
