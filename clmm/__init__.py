83622021687c8a67bd66afbdf5129f00d7a2c62b
Michel Aguena <aguena@lapp.in2p3.fr>
2020-12-16 10:21:16 +0100

    readd lsst files

--- clmm/__init__.py
+++ clmm/__init__.py
@@ -5,6 +5,7 @@ from .dataops import compute_tangential_and_cross_components, make_radial_profil
 from .utils import compute_radial_averages, make_bins, convert_units
 from .theory import get_reduced_shear_from_convergence, get_3d_density, predict_surface_density, predict_excess_surface_density, get_critical_surface_density, predict_tangential_shear, predict_convergence, predict_reduced_tangential_shear, Modeling, Cosmology
 from . import support
+from . import lsst
 
 
 __version__ = '0.8.0'
---
+++ clmm/lsst/__init__.py
@@ -0,0 +1,1 @@
+from .load_clusters_with_gcr import *
