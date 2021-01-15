185e7ee71bcef4f9971ba4d287f9723d1e9bf70b
Michel Aguena <aguena@lapp.in2p3.fr>
2021-01-15 21:57:32 +0100

    move theory backends configs to clmm/theory/be_setup.py and add unittests

--- clmm/theory/__init__.py
+++ clmm/theory/__init__.py
@@ -3,6 +3,7 @@
 import importlib
 import warnings
 import os
+from clmm.theory import be_setup
 
 #  Preload functions:
 #    Some backends depend on more complicated modules and thus on a preload
@@ -15,34 +16,11 @@ def __numcosmo_preload():
     gi.require_version("NumCosmo", "1.0")
 
 
-#  Backend dictionary __backends:
-#    Dictonary controling the backends, it must test if the backend is available
-#    and loadable.
-#    - name: The backend name;
-#    - module: The actual module name, must be a .py file inside the modbackend
-#      directory;
-#    - prereqs: modules that need to be loadable to allow the backend to work;
-#    - preload: an optional function that must be called before the modules in
-#      prereqs are tested;
-#    - available: must always starts False;
-__backends = {'ct':  {'name': 'cluster_toolkit+astropy', 'available': False,
-                      'module': 'cluster_toolkit',
-                      'prereqs': ['cluster_toolkit', 'astropy']},
-              'nc':  {'name': 'NumCosmo', 'available': False,
-                      'module': 'numcosmo',
-                      'prereqs': ['gi.repository.NumCosmoMath', 'gi.repository.NumCosmo'],
-                      'preload': __numcosmo_preload},
-              'ccl': {'name': 'ccl', 'available': False,
-                      'module': 'ccl',
-                      'prereqs': ['pyccl']},
-              'notabackend': {'name': 'notaname', 'available': False,
-                              'module': 'notamodule',
-                              'prereqs': ['notaprerq']}}
 
 #  Backend check:
 #    Checks all backends and set available to True for those that can be
 #    corretly loaded.
-for _, be in __backends.items():
+for _, be in be_setup.__backends.items():
     try:
         if 'preload' in be:
             be['preload']()
@@ -56,26 +34,26 @@ for _, be in __backends.items():
 #    If the environment variable CLMM_MODELING_BACKEND is set it gets its value,
 #    falls back to 'ct' => cluster_toolkit if CLMM_MODELING_BACKEND is not set.
 be_nick = os.environ.get('CLMM_MODELING_BACKEND', 'ct')
-if not be_nick in __backends:
+if not be_nick in be_setup.__backends:
     raise ValueError("CLMM Backend `%s' is not supported" %(be_nick))
 
 #  Backend load:
 #  Loads the backend of choice if available or send a warning and try to load
 #  the backends in the order of the dictionary above.
 
-if not __backends[be_nick]['available']:
-    warnings.warn("CLMM Backend requested `%s' is not available, trying others..." %(__backends[be_nick]['name']))
+if not be_setup.__backends[be_nick]['available']:
+    warnings.warn("CLMM Backend requested `%s' is not available, trying others..." %(be_setup.__backends[be_nick]['name']))
     loaded = False
-    for be1 in __backends:
-        if __backends[be1]['available']:
-            backend = importlib.import_module("clmm.theory."+__backends[be1]['module'])
+    for be1 in be_setup.__backends:
+        if be_setup.__backends[be1]['available']:
+            backend = importlib.import_module("clmm.theory."+be_setup.__backends[be1]['module'])
             loaded = True
             be_nick = be1
             break
     if not loaded:
         raise ImportError("No modeling backend available.")
 else:
-    backend = importlib.import_module("clmm.theory."+__backends[be_nick]['module'])
+    backend = importlib.import_module("clmm.theory."+be_setup.__backends[be_nick]['module'])
 
 #  Import all backend symbols:
 #    Updates __all__ with the exported symbols from the backend and
@@ -93,7 +71,7 @@ except NotImplementedError:
 
 
 def backend_is_available(be1):
-    if not be1 in __backends:
+    if not be1 in be_setup.__backends:
         raise ValueError("CLMM Backend `%s' is not supported" %(be1))
     else:
-        return __backends[be1]['available']
+        return be_setup.__backends[be1]['available']
--- tests/test_theory_be.py
+++ tests/test_theory_be.py
@@ -1,9 +1,30 @@
 """Tests for backend of theory.py"""
+import os
 import importlib
 from numpy.testing import assert_raises
 
 def test_base(monkeypatch):
     """ Unit tests back end fails """
-    monkeypatch.setenv("CLMM_MODELING_BACKEND", "not_available_be")
     import clmm
+    # Unknown backend required
+    monkeypatch.setenv("CLMM_MODELING_BACKEND", "not_available_be")
     assert_raises(ValueError, importlib.reload, clmm.theory)
+    # no backends available
+    monkeypatch.setenv("CLMM_MODELING_BACKEND", "notabackend")
+    clmm.theory.be_setup.__backends = {
+              'notabackend': {'name': 'notaname', 'available': False,
+                              'module': 'notamodule',
+                              'prereqs': ['notaprerq']}}
+    assert_raises(ImportError, importlib.reload, clmm.theory)
+    # broken backend
+    clmm.theory.be_setup.__backends = {
+              'notabackend': {'name': 'notaname', 'available': True,
+                              'module': 'be_setup',
+                              'prereqs': ['notaprerq']}}
+    del clmm.theory.Modeling
+    try:
+        importlib.reload(clmm.theory)
+        module_problem = False
+    except:
+        module_problem = True
+    assert(module_problem)
