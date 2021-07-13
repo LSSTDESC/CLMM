cd0b3c3babd64f314ca963f01f60877c98141f67
Marina Ricci <rm003211@gmail.com>
2021-07-13 17:05:36 +0200

    add kwargs option in fitter

--- clmm/support/sampler.py
+++ clmm/support/sampler.py
@@ -17,19 +17,15 @@ def basinhopping(model_to_shear_profile, logm_0, args) :
 
     return spo.basinhopping(model_to_shear_profile, logm_0, minimizer_kwargs={'args':args}).x[0]
 
-def scicurve_fit(profile_model,radius,profile,err_profile,bounds=None,p0=None):
+
+def scicurve_fit(profile_model,radius,profile,err_profile,**kwargs):
     '''Uses scipy.optimize.curve_fit to find best fit parameters'''
+    
     from scipy import optimize as spo
 
-    if bounds is None:
-        return spo.curve_fit(profile_model,
+    return spo.curve_fit(profile_model,
                     radius, profile,
-                    sigma=err_profile, p0=p0)
-    else:
-        return spo.curve_fit(profile_model,
-                    radius, profile,
-                    sigma=err_profile, bounds=bounds, p0=p0)
-
+                    sigma=err_profile, **kwargs)
 
 
 
