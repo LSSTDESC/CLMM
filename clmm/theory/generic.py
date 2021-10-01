617506bfba1c1bee69dab84fc36367baa984c662
Marina Ricci <rm003211@gmail.com>
2021-10-01 18:05:23 +0200

    include magnification bias in generic.py

--- clmm/theory/generic.py
+++ clmm/theory/generic.py
@@ -27,3 +27,34 @@ def compute_reduced_shear_from_convergence(shear, convergence):
     """
     reduced_shear = np.array(shear)/(1.-np.array(convergence))
     return reduced_shear
+
+
+def compute_magnification_bias(magnification, alpha):
+    r""" Calculates magnification_bias from magnification and alpha parameter as :
+    .. math::
+     \mu^{\alpha - 1}
+    The alpha parameter depends on the source sample and is computed as the slope of the 
+    cummulative numer counts at a given magnitude:
+    .. math::
+        \alpha =    \alpha \equiv \alpha(f) = - \frac{\rm{d}}{{\rm{dlog}}f} \log n_0(>f)
+        
+    or,
+    .. math::    
+        \alpha \equiv \alpha(m) = 2.5 \frac{\rm{d}}{\rm{dm}} \log n_0(<m)
+    
+    see e.g.  Bartelmann & Schneider 2001; Umetsu 2020
+    
+    Parameters
+    ----------
+    magnification : array_like
+        Magnification
+    alpha : float
+        Source cummulative number density slope
+
+    Returns
+    -------
+    compute_magnification_bias : array_like
+        magnification bias
+    """
+    magnification_bias = magnification**(alpha_slope - 1)
+    return magnification_bias
