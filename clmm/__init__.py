20c897ab12e07c4cbe5524d7c4fe4c7355217e56
Marina Ricci <rm003211@gmail.com>
2021-10-27 18:23:02 +0200

    update doc

--- clmm/__init__.py
+++ clmm/__init__.py
@@ -4,10 +4,7 @@ from .galaxycluster import GalaxyCluster
 from .dataops import compute_tangential_and_cross_components, make_radial_profile
 from .utils import compute_radial_averages, make_bins, convert_units
 from .theory import (
-    compute_reduced_shear_from_convergence, compute_magnification_bias_from_magnification,  compute_3d_density, compute_surface_density,
-    compute_excess_surface_density, compute_critical_surface_density, compute_tangential_shear,
-    compute_convergence, compute_reduced_tangential_shear, compute_magnification, compute_magnification_bias, Modeling,
-    Cosmology
+    compute_reduced_shear_from_convergence, compute_magnification_bias_from_magnification, compute_3d_density, compute_surface_density, compute_excess_surface_density, compute_critical_surface_density, compute_tangential_shear, compute_convergence, compute_reduced_tangential_shear, compute_magnification, compute_magnification_bias, Modeling, Cosmology
 )
 from . import support
 
