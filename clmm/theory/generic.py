2f902b7ef7f281b6d7bd1f4fa568ad702baf4ce2
Marina Ricci <rm003211@gmail.com>
2021-10-27 18:14:26 +0200

    weird indent error

--- clmm/theory/generic.py
+++ clmm/theory/generic.py
@@ -5,55 +5,57 @@ Model independent theory functions
 
 import numpy as np
 
-__all__ = ['compute_reduced_shear_from_convergence', 'compute_magnification_bias_from_magnification']
+__all__ = ['compute_reduced_shear_from_convergence',
+           'compute_magnification_bias_from_magnification']
 
 # functions that are general to all backends
 
 
 def compute_reduced_shear_from_convergence(shear, convergence):
-	""" Calculates reduced shear from shear and convergence
+    """ Calculates reduced shear from shear and convergence
 
-	Parameters
-	----------
-	shear : array_like
-		Shear
-	convergence : array_like
-		Convergence
+    Parameters
+    ----------
+    shear : array_like
+            Shear
+    convergence : array_like
+            Convergence
 
-	Returns
-	-------
-	array_like
-		Reduced shear
-	"""
-	reduced_shear = np.array(shear)/(1.-np.array(convergence))
-	return reduced_shear
+    Returns
+    -------
+    array_like
+            Reduced shear
+    """
+    reduced_shear = np.array(shear)/(1.-np.array(convergence))
+    return reduced_shear
 
 
 def compute_magnification_bias_from_magnification(magnification, alpha):
-	r""" Calculates magnification_bias from magnification and alpha parameter as :
-	.. math::
-	\mu^{\alpha - 1}
-	The alpha parameter depends on the source sample and is computed as the slope of the 
-	cummulative numer counts at a given magnitude:
-	.. math::
-		\alpha =    \alpha \equiv \alpha(f) = - \frac{\rm{d}}{{\rm{dlog}}f} \log n_0(>f)
-        
-	or,
-	.. math::    
-		\alpha \equiv \alpha(m) = 2.5 \frac{\rm{d}}{\rm{dm}} \log n_0(<m)
-	see e.g.  Bartelmann & Schneider 2001; Umetsu 2020
+    r""" Calculates magnification_bias from magnification and alpha parameter as :
+    .. math::
+    \mu^{\alpha - 1}
+    The alpha parameter depends on the source sample and is computed as the slope of the 
+    cummulative numer counts at a given magnitude:
+    .. math::
+            \alpha =    \alpha \equiv \alpha(f) = - \frac{\rm{d}}{{\rm{dlog}}f} \log n_0(>f)
 
-	Parameters
-	----------
-	magnification : array_like
-		Magnification
-	alpha : array like
-		Source cummulative number density slope
+    or,
+    .. math::    
+            \alpha \equiv \alpha(m) = 2.5 \frac{\rm{d}}{\rm{dm}} \log n_0(<m)
+    see e.g.  Bartelmann & Schneider 2001; Umetsu 2020
 
-	Returns
-	-------
-	compute_magnification_bias : array_like
-		magnification bias
-	"""
-	magnification_bias_from_magnification = np.array(magnification)**(np.array([alpha]).T - 1)
-	return magnification_bias_from_magnification
+    Parameters
+    ----------
+    magnification : array_like
+            Magnification
+    alpha : array like
+            Source cummulative number density slope
+
+    Returns
+    -------
+    compute_magnification_bias : array_like
+            magnification bias
+    """
+    magnification_bias_from_magnification = np.array(
+        magnification)**(np.array([alpha]).T - 1)
+    return magnification_bias_from_magnification
