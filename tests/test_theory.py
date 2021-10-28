ce5c8c9836f924fb5f83bc810edfa0264f1f0a84
Marina Ricci <rm003211@gmail.com>
2021-10-28 13:41:13 +0200

    decomment tests

--- tests/test_theory.py
+++ tests/test_theory.py
@@ -431,8 +431,8 @@ def test_shear_convergence_unittests(modeling_data):
     alpha = 3.78
     assert_allclose(theo.compute_magnification_bias(cosmo=cosmo, **cfg['GAMMA_PARAMS'], alpha=alpha),
                     (1./((1-kappa)**2-abs(gammat)**2))**(alpha - 1), 1.0e-10)
-#    assert_allclose((1./((1-kappa)**2-abs(gammat)**2))**(alpha - 1),
-#                    cfg['numcosmo_profiles']['mu']**(alpha - 1), 1.e2*reltol)
+    assert_allclose((1./((1-kappa)**2-abs(gammat)**2))**(alpha - 1),
+                    cfg['numcosmo_profiles']['mu']**(alpha - 1), 1.e2*reltol)
     
     # Check that shear, reduced shear and convergence return zero and magnification and magnification bias returns one if
     # source is in front of the cluster
@@ -530,8 +530,8 @@ def test_shear_convergence_unittests(modeling_data):
     alpha = -1.78
     assert_allclose(mod.eval_magnification_bias(*profile_pars, alpha=alpha),
                     1./((1-kappa)**2-abs(gammat)**2)**(alpha-1), 1.0e-10)
-#    assert_allclose(1./((1-kappa)**2-abs(gammat)**2)**(alpha-1),
-#                    cfg['numcosmo_profiles']['mu']**(alpha-1), 1.e2*reltol)
+    assert_allclose(1./((1-kappa)**2-abs(gammat)**2)**(alpha-1),
+                    cfg['numcosmo_profiles']['mu']**(alpha-1), 1.e2*reltol)
     
     # Check that shear, reduced shear and convergence return zero and magnification returns one if
     # source is in front of the cluster
