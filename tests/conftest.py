03378a4995c1a80fd47a55249d27d98a2ff0dff2
Celine Combet <celine.combet@lpsc.in2p3.fr>
2021-11-18 11:39:30 +0100

    Update conf tests to also run on Einasto profile + adjust tolerance values

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,9 +6,9 @@ import importlib
 import clmm
 
 #@pytest.fixture(scope="module", params=["ccl", "nc", "ct", "notabackend", "testnotabackend"])
-@pytest.fixture(scope="module", params=[{'nick': 'ccl', 'cosmo_reltol': 8.0e-8, 'dataops_reltol': 3.0e-8, 'theory_reltol': 2.0e-6}, 
-                                        {'nick': 'nc',  'cosmo_reltol': 1.0e-8, 'dataops_reltol': 1.0e-8, 'theory_reltol': 1.0e-8},  
-                                        {'nick': 'ct',  'cosmo_reltol': 1.0e-5, 'dataops_reltol': 5.0e-6, 'theory_reltol': 3.5e-3},
+@pytest.fixture(scope="module", params=[{'nick': 'ccl', 'cosmo_reltol': 8.0e-8, 'dataops_reltol': 3.0e-8, 'theory_reltol': 2.0e-6, 'theory_reltol_num': 2.0e-3}, 
+                                        {'nick': 'nc',  'cosmo_reltol': 1.0e-8, 'dataops_reltol': 1.0e-8, 'theory_reltol': 1.0e-8, 'theory_reltol_num': 1.0e-8},  
+                                        {'nick': 'ct',  'cosmo_reltol': 1.0e-5, 'dataops_reltol': 5.0e-6, 'theory_reltol' : 3.5e-3, 'theory_reltol_num': 3.5e-3},
                                         {'nick': 'notabackend', 'cosmo_reltol': 8.0e-8, 'dataops_reltol': 3.0e-8, 'theory_reltol': 2.0e-6},
                                         {'nick': 'testnotabackend', 'cosmo_reltol': 0.0, 'theory_reltol': 0.0} ])
 def modeling_data(request):
@@ -37,7 +37,8 @@ def cosmo_init(request):
 
     return param
 
-@pytest.fixture(scope="module", params=['nfw', 'einasto', 'hernquist'])
+@pytest.fixture(scope="module", params=['nfw', 'einasto'])
+#@pytest.fixture(scope="module", params=['nfw'])
 def profile_init(request):
     param = request.param
 
