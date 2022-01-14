744aaa585f0f4a4b62cb1aa65e8f43169968d12d
payerne <constantin.payerne@etu.univ-grenoble-alpes.fr>
2022-01-14 18:14:00 +0100

    minor changes

--- clmm/clusterensemble.py
+++ clmm/clusterensemble.py
@@ -137,13 +137,13 @@ class ClusterEnsemble():
             components_error=[None if n is None else galaxycluster.galcat[n].data
                               for n in (tan_component_in_err, cross_component_in_err, None)],
             )
-        #profile_table[weights_out] = 1 rm this line
+        profile_table[weights_out] = 1 #rm this line
         if bin_units != 'radians':
             bins = convert_units(bins, bin_units, 'radians', redshift=galaxycluster.z, cosmo=cosmo)
-        profile_table[weights_out] = binned_statistic(galaxycluster.galcat['theta'], 
-                                                       galaxycluster.galcat[weights_in], 
-                                                       statistic='sum', 
-                                                       bins = bins)[0]
+        #profile_table[weights_out] = binned_statistic(galaxycluster.galcat['theta'], 
+        #                                               galaxycluster.galcat[weights_in], 
+        #                                               statistic='sum', 
+        #                                               bins = bins)[0]
         data_to_save = [galaxycluster.unique_id, galaxycluster.ra, galaxycluster.dec, galaxycluster.z,
                         *[np.array(profile_table[col]) for col in
                             ('radius', 'p_0', 'p_1', weights_out)]]
