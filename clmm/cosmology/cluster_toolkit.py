c61fac8d5b9b4aeafaed6969aba19e5fdd76492a
Hsin Fan <57552401+hsinfan1996@users.noreply.github.com>
2022-01-19 17:48:27 +0800

    Fix typo

--- clmm/cosmology/cluster_toolkit.py
+++ clmm/cosmology/cluster_toolkit.py
@@ -80,7 +80,7 @@ class AstroPyCosmology(CLMMCosmology):
         return self.be_cosmo.Om(z)*(self.be_cosmo.H(z)/self.be_cosmo.H0)**2
 
     def _get_rho_crit(self, z):
-        return self.be_csomo.critical_density(z).to(u.Msun/u.Mpc**3).value
+        return self.be_cosmo.critical_density(z).to(units.Msun/units.Mpc**3).value
 
     def _eval_da_z1z2(self, z1, z2):
         return self.be_cosmo.angular_diameter_distance_z1z2(z1, z2).to_value(units.Mpc)
