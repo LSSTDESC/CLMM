fc6c89df2fe5f94f3551da3f71589cb55e781eaa
Hsin Fan <57552401+hsinfan1996@users.noreply.github.com>
2022-03-21 16:41:40 +0800

    Vectorize _get_E2 for the NC backend

--- clmm/cosmology/numcosmo.py
+++ clmm/cosmology/numcosmo.py
@@ -112,7 +112,7 @@ class NumCosmoCosmology(CLMMCosmology):
 
     def _get_E2(self, z):
 
-        return self.be_cosmo.E2(z)
+        return np.vectorize(self.be_cosmo.E2)(z)
 
     def _get_E2Omega_m(self, z):
 
