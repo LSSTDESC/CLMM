60e7b812b45167062de7e852da348b689843ed7e
Hsin Fan <57552401+hsinfan1996@users.noreply.github.com>
2022-06-04 03:08:16 +0800

    Issue/507/ccl_eval_da (#510)
    * Patch _eval_da_z1z2 for CCL backend
    
    * Update _eval_sigma_crit for CCL backend
    
    * add compute_for_good_redshifts function
    
    * Rename _eval_da_z1z2 to _eval_da_z1z2_core
    
    * Rename _eval_sigma_crit to _eval_sigma_crit_core
    
    * Update error_message for _eval_da_z1z2
    
    * Update compute_for_good_redshifts docstrings
    
    * Update validate_argument
    
    * Replace repeat with full_like for speedup
    
    * update verion to 1.1.10
    
    Co-authored-by: Michel Aguena <aguena@lapp.in2p3.fr>
    Co-authored-by: m-aguena <michel.aguena@gmail.com>

--- clmm/cosmology/cluster_toolkit.py
+++ clmm/cosmology/cluster_toolkit.py
@@ -82,22 +82,17 @@ class AstroPyCosmology(CLMMCosmology):
     def _get_E2Omega_m(self, z):
         return self.be_cosmo.Om(z)*(self.be_cosmo.H(z)/self.be_cosmo.H0)**2
 
-    def _eval_da_z1z2(self, z1, z2):
+    def _eval_da_z1z2_core(self, z1, z2):
         return self.be_cosmo.angular_diameter_distance_z1z2(z1, z2).to_value(units.Mpc)
 
-    def _eval_sigma_crit(self, z_len, z_src):
-        if np.any(np.array(z_src) <= z_len):
-            warnings.warn(
-                'Some source redshifts are lower than the cluster redshift. '
-                'Returning Sigma_crit = np.inf for those galaxies.')
+    def _eval_sigma_crit_core(self, z_len, z_src):
         # Constants
         clight_pc_s = const.CLIGHT_KMS.value*1000./const.PC_TO_METER.value
         gnewt_pc3_msun_s2 = const.GNEWT.value * \
             const.SOLAR_MASS.value/const.PC_TO_METER.value**3
 
-        d_l = self.eval_da_z1z2(0, z_len)
-        d_s = self.eval_da_z1z2(0, z_src)
-        d_ls = self.eval_da_z1z2(z_len, z_src)
+        d_l = self._eval_da_z1z2_core(0, z_len)
+        d_s = self._eval_da_z1z2_core(0, z_src)
+        d_ls = self._eval_da_z1z2_core(z_len, z_src)
 
-        beta_s = np.maximum(0., d_ls/d_s)
-        return clight_pc_s**2/(4.0*np.pi*gnewt_pc3_msun_s2)*1/d_l*np.divide(1., beta_s)*1.0e6
+        return clight_pc_s**2/(4.0*np.pi*gnewt_pc3_msun_s2)*d_s/(d_l*d_ls)*1.0e6
