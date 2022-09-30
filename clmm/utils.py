894df7184a04e55453254980972bfe018df1f027
Hsin Fan <57552401+hsinfan1996@users.noreply.github.com>
2022-09-30 23:21:24 +0800

    Minor fix

--- clmm/utils.py
+++ clmm/utils.py
@@ -114,7 +114,7 @@ def correct_sigma_with_boost_model(rvals, sigma_vals, boost_model='nfw_boost', *
     return sigma_corrected
 
 def compute_radial_averages(xvals, yvals, xbins, yerr=None, error_model='ste', weights=None):
-    """ Given a list of `xvals`, `yvals` and bins, sort into bins. If `xvals` or `yvals`
+    """ Given a list of `xvals`, `yvals` and `xbins`, sort into bins. If `xvals` or `yvals`
     contain non-finite values, these are filtered.
 
     Parameters
