a4d7453d2be86daa2fd4ce9fd0db2f7e5b6ac581
miague <michel.aguena@gmail.com>
2023-03-30 08:33:57 +0000

    fix spaces

--- clmm/theory/ccl.py
+++ clmm/theory/ccl.py
@@ -128,6 +128,7 @@ class CCLCLMModeling(CLMModeling):
         else:
             a_cl = self.cosmo.get_a_from_z(z_cl)
         return self.hdpm._get_alpha(self.cosmo.be_cosmo, self.__mdelta_cor, a_cl, self.mdef)
+
     def _eval_3d_density(self, r3d, z_cl):
         """"eval 3d density"""
         return self._call_ccl_profile_lens(self.hdpm.real, r3d, z_cl, ndim=3)
