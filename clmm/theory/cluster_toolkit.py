dfcaf7c3440d16c87caccff8acf78bb3c8efae6d
Caio Lima de Oliveira <code.caio@limadeoliveira.me>
2025-09-23 11:28:00 -0300

    refactor: reorder import statements in theory/cluster_toolkit.py

--- clmm/theory/cluster_toolkit.py
+++ clmm/theory/cluster_toolkit.py
@@ -2,10 +2,10 @@
 Modeling using cluster_toolkit
 """
 
+import cluster_toolkit as ct
+
 # Functions to model halo profiles
 import numpy as np
-
-import cluster_toolkit as ct
 
 from ..cosmology.cluster_toolkit import AstroPyCosmology
 from ..utils import _patch_rho_crit_to_cd2018
