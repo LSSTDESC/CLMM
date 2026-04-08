2628f6c7dd563b7efb5d55db4f418c2780137012
Michel <aguena@lapp.in2p3.fr>
2026-04-08 17:27:03 +0200

    update DC2 cosmo vals for nbs

--- examples/Paper_v2.0/cosmology_set.py
+++ examples/Paper_v2.0/cosmology_set.py
@@ -1,3 +1,4 @@
 from clmm import Cosmology
 
-dc2_cosmo = Cosmology(H0=70.0, Omega_dm0=0.27 - 0.045, Omega_b0=0.045, Omega_k0=0.0)
+#dc2_cosmo = Cosmology(H0=70.0, Omega_dm0=0.27 - 0.045, Omega_b0=0.045, Omega_k0=0.0)
+dc2_cosmo = Cosmology(H0=71.0, Omega_dm0=0.265-0.0448, Omega_b0=0.0448, Omega_k0=0.0)
